a1541048a2782832c0e094034ea72d4b33c04031
ajpadilla <ajpadilla88@gmail.com>
2015-04-09 12:38:26 -0430

    Agregando relación foranea con las tablas posiciones y paises

--- app/database/migrations/2015_04_09_164223_create_table_jugadores.php
+++ app/database/migrations/2015_04_09_164223_create_table_jugadores.php
@@ -15,8 +15,31 @@ class CreateTableJugadores extends Migration {
 		Schema::create('jugadores', function($table){
 			$table->increments('id');
 			$table->string('nombre', 128);
+			$table->date('fecha_nacimiento');
+			$table->string('foto', 128)->nullable(true)->default(null);
+			$table->decimal('altura', 10, 2)->nullable(true)->default(null);
 			$table->string('abreviacion', 20);
 			$table->timestamps();
+		});
+
+		Schema::table('jugadores', function(Blueprint $table)
+		{
+				$table->integer('posicion_id')->unsigned()->after('abreviacion');	
+				$table->foreign('posicion_id')
+						->references('id')	
+						->on('posiciones')
+						->onDelete('no action')
+						->onUpdate('cascade');	
+		});
+
+		Schema::table('jugadores', function(Blueprint $table)
+		{
+				$table->integer('pais_id')->unsigned()->after('posicion_id');	
+				$table->foreign('pais_id')
+						->references('id')	
+						->on('paises')
+						->onDelete('no action')
+						->onUpdate('cascade');	
 		});
 	}
 
@@ -27,6 +50,13 @@ class CreateTableJugadores extends Migration {
 	 */
 	public function down()
 	{
+		Schema::table('jugadores', function(Blueprint $table)
+		{
+			$table->dropForeign('jugadores_posicion_id_foreign');
+			$table->dropColumn('posicion_id');
+			$table->dropForeign('jugadores_pais_id_foreign');
+			$table->dropColumn('pais_id');
+		});
 		Schema::dropIfExists('jugadores');
 	}
 
