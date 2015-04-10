4660a6d5b22023249784045f88efdd5484980df3
ajpadilla <ajpadilla88@gmail.com>
2015-04-10 14:03:19 -0430

    Implementando paquete Laravel-Stapler, para cargar imagenes

--- app/Soccer/Jugador/Jugador.php
+++ app/Soccer/Jugador/Jugador.php
@@ -1,13 +1,29 @@
 <?php namespace soccer\Jugador;
 
 use Eloquent;
+use Codesleeve\Stapler\ORM\StaplerableInterface;
+use Codesleeve\Stapler\ORM\EloquentTrait;
 
 /**
 * 
 */
-class Jugador extends Eloquent{
-
+class Jugador extends Eloquent implements StaplerableInterface{
+	use EloquentTrait;
+	
 	protected $table = 'jugadores';
+
+	protected $fillable = ['nombre', 'fecha_nacimiento','foto','altura', 'abreviacion','posicion_id','pais_id'];
+
+	 public function __construct(array $attributes = array()) {
+        $this->hasAttachedFile('foto', [
+            'styles' => [
+                'medium' => '300x300',
+                'thumb' => '100x100'
+            ]
+        ]);
+
+        parent::__construct($attributes);
+    }
 
 	public function pais()
 	{
