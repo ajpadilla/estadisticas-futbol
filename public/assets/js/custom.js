ecef8cd33d9fcda2acdf02d6766e97e4884866fb
ajpadilla <ajpadilla88@gmail.com>
2015-04-10 17:35:25 -0430

    Agregando metodo editPlayer

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -238,6 +238,248 @@ var CustomApp = function () {
         });               
     }
 
+    // Metodo para editar datos del jugador
+    var editPlayer = function(idPlayer) {
+
+        $.validator.addMethod('onlyLettersNumbersAndSpaces', function(value, element) {
+                    return this.optional(element) || /^[a-zA-Z0-9ñÑ\s]+$/i.test(value);
+            }, 'sólo letras, números y espacios.');
+
+        $.validator.addMethod('customDateValidator', function(value, element) {
+            try{
+                jQuery.datepicker.parseDate('dd/mm/yy', value);return true;}
+            catch(e){return false;}
+        },'Por favor, Ingrese una fecha valida.');
+
+        jQuery.validator.addMethod('decimalNumbers', function(value, element) {
+            return this.optional(element) || /^\d{0,10}(\.\d{0,2})?$/i.test(value);
+        }, 'Por favor ingrese maximo'+[10]+'digitos enteros'+'y maximo'+[2]+'digitos decimales');
+
+        $.validator.addMethod('onlyLettersNumbersAndDash', function(value, element) {
+              return this.optional(element) || /^[a-zA-Z0-9ñÑ\-]+$/i.test(value);
+        }, 'sólo letras, números y guiones.');
+
+        $('#player-form').validate({
+            rules:{
+                nombre:{
+                    required:true,
+                    rangelength: [2, 64],
+                    onlyLettersNumbersAndSpaces: true
+                },
+                fecha_nacimiento:{
+                    required:true,
+                    //customDateValidator: true
+                },
+                altura:{
+                    required:true,
+                    decimalNumbers:true
+                },
+                abreviacion:{
+                    required:true,
+                    onlyLettersNumbersAndDash:true
+                },
+                posicion:{
+                    required:true,
+                },
+                pais:{
+                    required:true,
+                }
+            },
+            messages:{
+                nombre:{
+                    required:'Este campo es obligatorio.',
+                },
+                fecha_nacimiento:{
+                    required:'Este campo es obligatorio.',
+                },
+                altura:{
+                   required:'Este campo es obligatorio.',
+                },
+                abreviacion:{
+                  required:'Este campo es obligatorio.',
+                },
+                posicion:{
+                    required:'Este campo es obligatorio.',
+                },
+                pais:{
+                    required:'Este campo es obligatorio.',
+                }
+            },
+            highlight:function(element){
+                $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            },
+            unhighlight:function(element){
+                $(element).closest('.form-group').removeClass('has-error');
+            },
+            success:function(element){
+                element.addClass('valid').closest('.form-group').removeClass('has-error').addClass('has-success');
+            }
+        });
+    
+        $.ajax({
+            type: 'GET',
+            url: $('#datos-jugador').attr('href'),    
+            data: {'jugadorId': idPlayer},
+            dataType: "JSON",
+            success: function(response) {
+                if (response.success == true) {
+                    console.log(response)
+                    console.log(response.jugador)
+                    $('#jugador_id').val(response.jugador.id);
+                    $('#nombre').val(response.jugador.nombre);
+                    $('#fecha_nacimiento').val(response.jugador.fecha_nacimiento);
+                    $('#altura').val(response.jugador.altura);
+                    $('#abreviacion').val(response.jugador.abreviacion);
+                    $('#posicion_id').val(response.jugador.posicion_id);
+                    $('#pais_id').val(response.jugador.pais_id);
+                    $('.chosen-select').trigger("chosen:updated");
+                }
+            }
+        });
+        
+        bootbox.dialog({
+                    message: $('#player-form-div'),
+                    buttons: {
+                        success: {
+                            label: "Actualizar",
+                            className: "btn-primary",
+                            callback: function () {
+                                // Si quieres usar aquí jqueryForm, es lo mismo, lo agregas y ya. Creo que es buena idea!
+
+                                //ajax para el envío del formulario.
+                                if($('#player-form').valid()) {
+
+                                    var response = false; // Esta variable debería recibir los datos por ajax.
+                                    var dataServer = null;
+
+                                    $("#player-form").submit(function(e){
+                                        var formData = new FormData(this);
+                                        //var form = $('#player-form').serializeArray();
+
+                                        $.ajax({
+                                            type: 'POST',
+                                            url: $('#editar-jugador').attr('href'), 
+                                            data: formData,
+                                            contentType: false,
+                                            processData: false,
+                                            dataType: "JSON",
+                                            success: function(responseServer) {
+                                                if(responseServer.success == true) 
+                                                {
+                                                    dataServer = responseServer;
+                                                    console.log(dataServer);
+                                                    // Muestro otro dialog con información de éxito
+                                                    bootbox.dialog({
+                                                        message: $('#nombre').val() + " ha sido Actualizado correctamente!",
+                                                        title: "Éxito",
+                                                        buttons: {
+                                                            success: {
+                                                                label: "Success!",
+                                                                className: "btn-success"
+                                                            }
+                                                        }
+                                                    });
+                                                    // Limpio cada elemento de las clases añadidas por el validator
+                                                    $('#player-form div').each(function(){
+                                                        cleanValidatorClasses(this);
+                                                    });
+                                                    //Reinicio el formulario
+                                                    $("#player-form")[0].reset();
+                                                    $('.chosen-select').html("");
+                                                    loadFieldSelect($('#lista-paises').attr('href'),'#pais_id');
+                                                    loadFieldSelect($('#lista-posiciones').attr('href'),'#posicion_id');
+                                                    $('.chosen-select').trigger("chosen:updated");
+                                                }else{
+                                                     bootbox.dialog({
+                                                        message:" ¡Error al agregar datos!",
+                                                        title: "Error",
+                                                        buttons: {
+                                                            danger: {
+                                                                label: "Danger!",
+                                                                className: "btn-danger"
+                                                            }
+                                                        }
+                                                    });
+                                                    // Limpio cada elemento de las clases añadidas por el validator
+                                                    $('#player-form div').each(function(){
+                                                        cleanValidatorClasses(this);
+                                                    });
+                                                    //Reinicio el formulario
+                                                    $("#player-form")[0].reset();
+                                                    $('.chosen-select').html("");
+                                                    $('.chosen-select').trigger("chosen:updated");
+                                                }
+                                            },
+                                            error: function(jqXHR, textStatus, errorThrown) {
+                                               bootbox.dialog({
+                                                        message:" ¡Error al enviar datos al servidor!",
+                                                        title: "Error",
+                                                        buttons: {
+                                                            danger: {
+                                                                label: "Danger!",
+                                                                className: "btn-danger"
+                                                            }
+                                                        }
+                                                    });
+                                                    // Limpio cada elemento de las clases añadidas por el validator
+                                                    $('#player-form div').each(function(){
+                                                        cleanValidatorClasses(this);
+                                                    });
+                                                    //Reinicio el formulario
+                                                    $("#player-form")[0].reset();
+                                                    $('.chosen-select').html("");
+                                                    loadFieldSelect($('#lista-paises').attr('href'),'#pais_id');
+                                                    loadFieldSelect($('#lista-posiciones').attr('href'),'#posicion_id');
+                                                    $('.chosen-select').trigger("chosen:updated");
+                                            }
+                                        });
+                                        e.preventDefault(); //Prevent Default action. 
+                                    }); 
+                                    $("#player-form").submit();
+                                } else {
+                                    return false;
+                                }
+
+                            }
+                        }
+                    },
+                    show: false // We will show it manually later
+                })
+                .on('shown.bs.modal', function() {
+                    $('#player-form-div')
+                        .show();                             // Show the form
+            })
+            .on('hide.bs.modal', function(e) {
+                // Bootbox will remove the modal (including the body which contains the form)
+                // after hiding the modal
+                // Therefor, we need to backup the form
+                $('#player-form-div').hide().appendTo('#new-player-form');
+            })
+            .modal('show');
+    }
+
+    // Metodo para saber cual opcion(ver, editar, borrar) fue seleccionada
+    var loadDataPlayer = function() 
+    {
+        $('.table').click(function(event)
+        {
+            var target = $( event.target );
+            if (target.is('a')) 
+            {
+                console.log($(target).attr('id'));
+
+                var id = $(target).attr('id');
+                var type = id ? id.split('_')[0] : '';
+                var numberId = id ? id.split('_')[1] : '';
+
+                if (type == "editar") 
+                {
+                    editPlayer(numberId);
+                }
+            }           
+        });
+    }
+
     /*-----------------------------------------------------------------------------------*/
     /*  Fancybox New Player
     /*-----------------------------------------------------------------------------------*/
@@ -310,6 +552,7 @@ var CustomApp = function () {
             handleBootboxNewPlayer();
             loadFieldSelect($('#lista-paises').attr('href'),'#pais_id');
             loadFieldSelect($('#lista-posiciones').attr('href'),'#posicion_id');
+            loadDataPlayer();
         }
     };
 }();
