498e0e404b5a6391f4e63af2e847db349846b849
ajpadilla <ajpadilla88@gmail.com>
2015-04-11 00:19:16 -0430

    Agregando formato para validar fecha_nacimiento

--- app/Soccer/Forms/EditarJugadorForm.php
+++ app/Soccer/Forms/EditarJugadorForm.php
@@ -5,7 +5,7 @@ use Laracasts\Validation\FormValidator;
 class EditarJugadorForm extends FormValidator{
         protected $rules = [
           'nombre' => 'required|max:128',
-          'fecha_nacimiento' => 'required', 
+          'fecha_nacimiento' => 'required|date_format:d-m-Y', 
           'altura' => 'required|numeric',
           'abreviacion' => 'required|max:20',
           'posicion_id' => 'required|integer',
