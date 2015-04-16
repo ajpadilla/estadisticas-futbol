42b832d4321ce488af99b9698f99119bcc4db6d8
ajpadilla <ajpadilla88@gmail.com>
2015-04-16 17:27:50 -0430

    Editando datos del pais seleccionado

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -871,6 +871,186 @@ var CustomApp = function () {
         });
     }
 
+    // Metodo para editar datos del país
+    var editCountry = function(idCountry) {
+
+        $.validator.addMethod('onlyLettersNumbersAndSpaces', function(value, element) {
+                    return this.optional(element) || /^[a-zA-Z0-9ñÑ\s]+$/i.test(value);
+            }, 'sólo letras, números y espacios.');
+
+        $.validator.addMethod('customDateValidator', function(value, element) {
+            try{
+                jQuery.datepicker.parseDate('dd-mm-yy', value);return true;}
+            catch(e){return false;}
+        },'Por favor, Ingrese una fecha valida.');
+
+        jQuery.validator.addMethod('decimalNumbers', function(value, element) {
+            return this.optional(element) || /^\d{0,10}(\.\d{0,2})?$/i.test(value);
+        }, 'Por favor ingrese maximo'+[10]+'digitos enteros'+'y maximo'+[2]+'digitos decimales');
+
+        $.validator.addMethod('onlyLettersNumbersAndDash', function(value, element) {
+              return this.optional(element) || /^[a-zA-Z0-9ñÑ\-]+$/i.test(value);
+        }, 'sólo letras, números y guiones.');
+
+      $('#country-form').validate({
+            rules:{
+                nombre:{
+                    required:true,
+                    rangelength: [2, 128],
+                    onlyLettersNumbersAndSpaces: true
+                },
+                 bandera:{
+                    required:true,
+                    rangelength: [2, 128],
+                    onlyLettersNumbersAndSpaces: true
+                }
+            },
+            messages:{
+                nombre:{
+                    required:'Este campo es obligatorio.',
+                },
+                bandera:{
+                    required:'Este campo es obligatorio.',
+                }
+            },
+            highlight:function(element){
+                $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            },
+            unhighlight:function(element){
+                $(element).closest('.form-group').removeClass('has-error');
+            },
+            success:function(element){
+                element.addClass('valid').closest('.form-group').removeClass('has-error').addClass('has-success');
+            }
+        });
+    
+        $.ajax({
+            type: 'GET',
+            url: $('#datos-pais').attr('href'),    
+            data: {'countryId': idCountry},
+            dataType: "JSON",
+            success: function(response) {
+                if (response.success == true) {
+                    console.log(response);
+                    console.log(response.pais);
+                    $('#pais_id').val(response.pais.id);
+                    $('#nombre').val(response.pais.nombre);
+                    $('#bandera').val(response.pais.bandera);
+                }
+            }
+        });
+        
+        bootbox.dialog({
+                    message: $('#country-form-div'),
+                    buttons: {
+                        success: {
+                            label: "Actualizar",
+                            className: "btn-primary",
+                            callback: function () {
+                                // Si quieres usar aquí jqueryForm, es lo mismo, lo agregas y ya. Creo que es buena idea!
+
+                                //ajax para el envío del formulario.
+                                if($('#country-form').valid()) {
+
+                                    var response = false; // Esta variable debería recibir los datos por ajax.
+                                    var dataServer = null;
+
+                                    $("#country-form").submit(function(e){
+                                        var formData = new FormData(this);
+                                        //var form = $('#country-form').serializeArray();
+
+                                        $.ajax({
+                                            type: 'POST',
+                                            url: $('#editar-pais').attr('href'), 
+                                            data: formData,
+                                            contentType: false,
+                                            processData: false,
+                                            dataType: "JSON",
+                                            success: function(responseServer) {
+                                                if(responseServer.success == true) 
+                                                {
+                                                    dataServer = responseServer;
+                                                    console.log(dataServer);
+                                                    // Muestro otro dialog con información de éxito
+                                                    bootbox.dialog({
+                                                        message: $('#nombre').val() + " ha sido Actualizado correctamente!",
+                                                        title: "Éxito",
+                                                        buttons: {
+                                                            success: {
+                                                                label: "Success!",
+                                                                className: "btn-success"
+                                                            }
+                                                        }
+                                                    });
+                                                    // Limpio cada elemento de las clases añadidas por el validator
+                                                    $('#country-form div').each(function(){
+                                                        cleanValidatorClasses(this);
+                                                    });
+                                                    //Reinicio el formulario
+                                                    $("#country-form")[0].reset();
+                                                }else{
+                                                     bootbox.dialog({
+                                                        message:" ¡Error al agregar datos!",
+                                                        title: "Error",
+                                                        buttons: {
+                                                            danger: {
+                                                                label: "Danger!",
+                                                                className: "btn-danger"
+                                                            }
+                                                        }
+                                                    });
+                                                    // Limpio cada elemento de las clases añadidas por el validator
+                                                    $('#country-form div').each(function(){
+                                                        cleanValidatorClasses(this);
+                                                    });
+                                                    //Reinicio el formulario
+                                                    $("#country-form")[0].reset();
+                                                }
+                                            },
+                                            error: function(jqXHR, textStatus, errorThrown) {
+                                               bootbox.dialog({
+                                                        message:" ¡Error al enviar datos al servidor!",
+                                                        title: "Error",
+                                                        buttons: {
+                                                            danger: {
+                                                                label: "Danger!",
+                                                                className: "btn-danger"
+                                                            }
+                                                        }
+                                                    });
+                                                    // Limpio cada elemento de las clases añadidas por el validator
+                                                    $('#country-form div').each(function(){
+                                                        cleanValidatorClasses(this);
+                                                    });
+                                                    //Reinicio el formulario
+                                                    $("#country-form")[0].reset();
+                                            }
+                                        });
+                                        e.preventDefault(); //Prevent Default action. 
+                                    }); 
+                                    $("#country-form").submit();
+                                } else {
+                                    return false;
+                                }
+
+                            }
+                        }
+                    },
+                    show: false // We will show it manually later
+                })
+                .on('shown.bs.modal', function() {
+                    $('#country-form-div')
+                        .show();                             // Show the form
+            })
+            .on('hide.bs.modal', function(e) {
+                // Bootbox will remove the modal (including the body which contains the form)
+                // after hiding the modal
+                // Therefor, we need to backup the form
+                $('#country-form-div').hide().appendTo('#new-country-form');
+            })
+            .modal('show');
+    }
+
     // Metodo para saber cual opcion(ver, editar, borrar) fue seleccionada de la lista de Paises
     var loadDataCountry = function() 
     {
@@ -889,7 +1069,7 @@ var CustomApp = function () {
                 if (view == "pais") 
                 {
                     if (type == "editar") {
-                        //editPlayer(numberId);
+                        editCountry(numberId);
                     }else if(type == "ver"){
                         viewDataCountry(numberId);
                     }else if(type == "eliminar"){
