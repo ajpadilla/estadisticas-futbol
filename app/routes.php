290b7351649c22a82756caa17077b86c44eaf218
ajpadilla <ajpadilla88@gmail.com>
2015-04-16 15:26:32 -0430

    Reorganizando rutas para cada modulo

--- app/routes.php
+++ app/routes.php
@@ -57,6 +57,16 @@ Route::get('equipos/api-lista', array('as'=>'equipos.api.lista', 'uses'=>'Equipo
 ********************************* RUTAS PARA PAISES ********************************
 */
 Route::get('lista-paises',  ['as' => 'paises.lista','uses' => 'PaisController@getAllValue' ] );
+Route::get('paises', ['as' => 'paises.index', 'uses' => 'PaisController@index']);
+Route::get('paises/nuevo', ['as' => 'paises.create', 'uses' => 'PaisController@create'] );
+Route::post('paises/guardar', ['as' => 'paises.store', 'uses' => 'PaisController@store' ] );
+Route::get('paises/ver/{id}', ['as' => 'paises.show', 'uses' => 'PaisController@show' ] );
+Route::get('paises/editar/{id}',  ['as' => 'paises.edit','uses' => 'PaisController@edit' ] );
+Route::post('paises/actualizar',  ['as' => 'paises.update','uses' => 'PaisController@update' ] );
+Route::get('paises/eliminar/{id}',  ['as' => 'paises.delete','uses' => 'PaisController@destroy' ] );
+
+Route::get('paises/api-ver/{id}',  ['as' => 'paises.api.show','uses' => 'PaisController@showApi' ] );
+Route::get('paises/api-lista', array('as'=>'paises.api.lista', 'uses'=>'PaisController@listaApi'));
 /*
 ********************************* RUTAS PARA POSICIONES ********************************
 */
