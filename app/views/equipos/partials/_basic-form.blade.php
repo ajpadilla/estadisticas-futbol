14978db73ea512acc9a8889918c0803bb2088bd2
ajpadilla <ajpadilla88@gmail.com>
2015-04-18 16:49:37 -0430

    Agregando id a cada campo del formulario

--- app/views/equipos/partials/_basic-form.blade.php
+++ app/views/equipos/partials/_basic-form.blade.php
@@ -14,20 +14,20 @@
 @endif
 <div class="form-group">
 	{{ Form::label('nombre', 'Nombre', ['class' => 'col-md-4 control-label']) }}	
-	<div class="col-md-8">{{ Form::text('nombre', null, ['class' => 'form-control']) }}</div>
+	<div class="col-md-8">{{ Form::text('nombre', null, ['class' => 'form-control','id' =>'nombre' ]) }}</div>
 </div>
 <div class="form-group">
 	{{ Form::label('apodo', 'Apodo', ['class' => 'col-md-4 control-label']) }}	
-	<div class="col-md-8">{{ Form::text('apodo', null, ['class' => 'form-control']) }}</div>
+	<div class="col-md-8">{{ Form::text('apodo', null, ['class' => 'form-control','id' => 'apodo']) }}</div>
 </div>
 <div class="form-group">
 	{{ Form::label('fecha_fundacion', 'Fecha de Fundación', ['class' => 'col-md-4 control-label']) }}	
-	<div class="col-md-8">{{ Form::text('fecha_fundacion', null, ['class' => 'form-control']) }}</div>
+	<div class="col-md-8">{{ Form::text('fecha_fundacion', null, ['class' => 'form-control','id' => 'fecha_fundacion']) }}</div>
 </div>
 <div class="form-group">
 	{{ Form::label('tipo','Tipo',['class'=>'col-md-4 control-label']) }}
 	<div class="col-md-8">
-		{{ Form::select('tipo', ['Selección', 'Club'], null, ['class' => 'form-control chosen-select','data-placeholder' => 'Selecciona el tipo']) }}
+		{{ Form::select('tipo', ['Selección', 'Club'], null, ['class' => 'form-control chosen-select','data-placeholder' => 'Selecciona el tipo','id'=> 'tipo']) }}
 	</div>
 </div>
 
