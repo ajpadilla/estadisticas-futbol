ce194cab6908f45657c562ee2265bae6fff77b24
ajpadilla <ajpadilla88@gmail.com>
2015-04-20 17:02:34 -0430

    Agregando reglas para validar equipos

---
+++ app/Soccer/Forms/EditarEquipoForm.php
@@ -0,0 +1,19 @@
+<?php namespace soccer\Forms;
+
+use Laracasts\Validation\FormValidator;
+
+class EditarEquipoForm extends FormValidator{
+        protected $rules = [
+          'nombre' => 'required|max:128',
+          'foto' => 'image',
+          'bandera' => 'image',
+          'escudo' => 'image',
+          'tipo' => 'required',
+          'fecha_fundacion' => 'required|date_format:d-m-Y', 
+          'apodo' => 'min:2|max:128',
+          'ubicacion' => 'min:2|max:512',
+          'historia' => 'min:2|max:512',
+          'info_url' =>'min:2|max:128!active_url',
+          'pais_id' => 'required|exists:paises,id'
+     ];
+}
