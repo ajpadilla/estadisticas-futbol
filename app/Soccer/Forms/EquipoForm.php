296a3ecd8239c68c119fa299210196e3321eea6a
ajpadilla <ajpadilla88@gmail.com>
2015-04-20 17:03:29 -0430

    Actualizandor reglas de validación

--- app/Soccer/Forms/EquipoForm.php
+++ app/Soccer/Forms/EquipoForm.php
@@ -4,14 +4,16 @@ use Laracasts\Validation\FormValidator;
 
 class EquipoForm extends FormValidator{
         protected $rules = [
-          'nombre' 	=> 'required|max:128',
-          //'bandera' => 'min:2',
-          //'escudo' 	=> 'min:2',
-          'tipo' 	=> 'required',
-          //'foto' => '',
+          'nombre' => 'required|max:128',
+          'foto' => 'image',
+          'bandera' => 'image',
+          'escudo' => 'image',
+          'tipo' => 'required',
           'fecha_fundacion' => 'required|date_format:d-m-Y', 
-          'apodo' 	=> 'min:2|max:128',
-          'ubicacion' 	=> 'min:2|max:512',
+          'apodo' => 'min:2|max:128',
+          'ubicacion' => 'min:2|max:512',
+          'historia' => 'min:2|max:512',
+          'info_url' =>'min:2|max:128',
           'pais_id' => 'required|exists:paises,id'
      ];
 }
