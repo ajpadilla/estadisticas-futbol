6515794829bb916f1fe9822af0e7c6686ff0eba2
Alvaro Padilla <ajpadilla88@gmail.com>
2015-04-28 11:28:58 -0430

    Agregando ruta, para traer datos de posición

--- app/routes.php
+++ app/routes.php
@@ -87,6 +87,7 @@ Route::get('paises/api-lista', array('as'=>'paises.api.lista', 'uses'=>'PaisCont
 Route::get('posiciones', ['as' => 'posiciones.index', 'uses' => 'PosicionController@index']);
 Route::post('posiciones/guardar', ['as' => 'posiciones.store', 'uses' => 'PosicionController@store' ] );
 
+Route::get('posiciones/api-ver',  ['as' => 'posiciones.data','uses' => 'PosicionController@showApi' ] );
 Route::get('posiciones/api-seleccionar',  ['as' => 'posiciones.seleccionar','uses' => 'PosicionController@getAllValue' ] );
 Route::get('posiciones/api-lista',  ['as' => 'posiciones.api.lista','uses' => 'PosicionController@listaApi' ] );
 
