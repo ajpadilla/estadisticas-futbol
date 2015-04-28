8e8abf72943ca1d3ea2686a7c884c12266b95ff8
nightzpy <nightzpy@gmail.com>
2015-04-28 05:39:31 +0000

    Migraciones para competencia y tipo de competencias

---
+++ app/database/migrations/2015_04_28_052948_create_competencias_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCompetenciasTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('competencias', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('nombre', 128);
+			$table->string('imagen', 128)->nullable();
+			$table->date('desde');
+			$table->date('hasta');
+			$table->integer('tipo_competencia_id');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('competencias');
+	}
+
+}
