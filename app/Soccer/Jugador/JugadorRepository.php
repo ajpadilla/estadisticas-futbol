0e6d2c1014708d6b1288662783c4cae0e8e470c4
Alvaro Padilla <ajpadilla88@gmail.com>
2015-04-30 17:38:26 -0430

    Actualizando metodo update

--- app/Soccer/Jugador/JugadorRepository.php
+++ app/Soccer/Jugador/JugadorRepository.php
@@ -32,47 +32,15 @@ class JugadorRepository extends BaseRepository
 
 	public function create($data = array())
 	{
-		$fechaNacimiento = $data['fecha_nacimiento'];
-		$data['fecha_nacimiento'] = Carbon::createFromFormat('d-m-Y', $fechaNacimiento)->format('Y-m-d');
-
 		$jugador = $this->model->create($data); 
-
 		return $jugador;
 	}
 
 	public function update($data = array())
 	{
-		$fechaNacimiento = $data['fecha_nacimiento'];
-		$data['fecha_nacimiento'] = Carbon::createFromFormat('d-m-Y', $fechaNacimiento)->format('Y-m-d');
-
-		/*$fechaInicio = Carbon::createFromFormat('d-m-Y', $data['fecha_inicio'])->format('Y-m-d');
-		if(!empty($data['fecha_fin'])){
-			$fechaFin = Carbon::createFromFormat('d-m-Y',  $data['fecha_fin'])->format('Y-m-d');
-		}else{
-			$fechaFin = null;
-		}*/
 
 		$jugador = $this->get($data['jugador_id']);
 		$jugador->update($data);
-
-		/*if(count($jugador->equipos()->whereEquipoId($data['equipo_id'])->whereFechaInicio($fechaInicio)->first()) > 0)
-		{
-			$jugador->equipos()->sync([$data['equipo_id'] => 
-				[
-					'numero' => $data['numero'],
-					'fecha_inicio' => $fechaInicio,
-					'fecha_fin' => $fechaFin
-				]
-			]);
-		}else{
-			$jugador->equipos()->attach($data['equipo_id'],
-				[
-				'numero' => $data['numero'],
-				'fecha_inicio' => $fechaInicio,
-				'fecha_fin' => $fechaFin
-				]
-			);
-		}*/
 
 		return $jugador;
 	}
