a3cbd5c7f399824413d9827f61a61d3bb2c8983b
Lenyn Alcántara <nightzpy@gmail.com>
2015-05-03 08:04:41 +0000

    Solucionando problemas con imagenes de equipos y competencias

--- app/views/competencias/profile-overview.blade.php
+++ app/views/competencias/profile-overview.blade.php
@@ -10,7 +10,7 @@
 					<div class="panel-body">
 						<div id="card" class="row">
 							<div class="col-md-4 headshot">
-								<img src="{{ $equipo->foto->url('medium') }}">
+								<img class="img-responsive" src="{{ $equipo->foto->url('medium') }}">
 							</div>
 							<div class="col-md-8">
 								<table class="table table-hover">
