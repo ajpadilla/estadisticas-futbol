39f0a9ead798ac4c81f209ba525d7b155a792975
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-07 16:05:25 -0430

    Eliminando carpeta jugadores

--- app/views/jugadores/partials/_jugadores-table.blade.php
+++
@@ -1,8 +0,0 @@
-{{ $table->render() }}
-@include('jugadores.new')
-@include('jugadores.view')
-@include('jugadores.partials._form_view-template')	
-
-@section('scripts')
-	{{ $table->script() }}
-@stop
--- app/views/jugadores/view.blade.php
+++
@@ -1,7 +0,0 @@
-@section('title-modal')
-	Datos Jugador
-@stop
-
-<div id="new-player-form-view" style="display:none">
-	@include('jugadores.partials._form_view')
-</div>
