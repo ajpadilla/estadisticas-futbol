3a59f809e997efdf12cdd8e2b39e64ba63844303
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-10 16:48:14 -0430

    Agregando balde

---
+++ app/views/groups/partials/_form.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+	<!-- BOX -->
+	<div  id="add-group-to-competition-form-div" class="box border primary col-md-12">
+		<div class="box-title">
+			<h4><i class="fa fa-user"></i><span class="hidden-inline-mobile">Agregar Nuevo Grupo</span></h4>
+		</div>
+		<div class="box-body">
+			{{ Form::open(['route' => ['competitions.api.add.group', $competition->id],'class'=>'form-horizontal','role'=>'form',
+			'method' => 'POST','files' => true,'id'=> 'add-group-to-competition-form']) }}
+			
+			{{ Form::close() }}
+		</div>
+	</div>
+	<!-- /BOX -->					
+</div>
