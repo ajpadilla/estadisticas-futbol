c68b4b2e4d9643c5e6d28a8ec1b0b27a6251c12e
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-15 01:06:03 -0430

    Validando campos para actualizar lista de equipos por grupos

---
+++ app/Soccer/Forms/RegisterTeamGroupForm.php
@@ -0,0 +1,10 @@
+<?php namespace soccer\Forms;
+
+use Laracasts\Validation\FormValidator;
+
+class RegisterTeamGroupForm extends FormValidator{
+        protected $rules = [
+          'group_id' => 'required|exists:groups,id',
+          'teams_ids' => 'required'
+     ];
+}
