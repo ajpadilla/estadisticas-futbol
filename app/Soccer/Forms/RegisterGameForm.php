389e3dd48110adaffa15d50ee87428fdba96c580
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-18 18:26:00 -0430

    Actualizando validaciones, para gregar nuevo juego

--- app/Soccer/Forms/RegisterGameForm.php
+++ app/Soccer/Forms/RegisterGameForm.php
@@ -4,15 +4,12 @@ use Laracasts\Validation\FormValidator;
 
 class RegisterGameForm extends FormValidator{
         protected $rules = [
-          'date' => 'date',
+          'date' => 'required',
           'local_team_id' => 'required|exists:equipos,id',
           'away_team_id' => 'required|exists:equipos,id',
           'type_id' => 'required|exists:game_types,id',
           'stadium' => 'between:2,128',
           'main_referee' => 'between:2,128',
           'line_referee' => 'between:2,128'
-          //'info_url' => 'url' ,
-          //'facebook_url' => 'url',
-          //'twitter_url' =>'url' 
      ];
 }
