73a3b9d74d0d9f50148cacb57c5bc6d09090b28c
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-20 16:31:40 -0430

    Agregando clase repository, para modelo GoalTypes

---
+++ app/Soccer/Game/Goal/GoalTypeRepository.php
@@ -0,0 +1,27 @@
+<?php namespace soccer\Game\Goal;
+
+use soccer\Game\Goal\GoalType;
+use soccer\Base\BaseRepository;
+use Carbon\Carbon;
+use Datatable;
+use Illuminate\Database\Eloquent\Collection;
+
+
+/**
+* 
+*/
+class GoalTypeRepository extends BaseRepository
+{
+	
+	function __construct() {
+		$this->columns = [];
+
+		$this->setModel(new GoalType);
+		$this->setListAllRoute('');
+	}
+
+	public function getAllForSelect()
+	{
+		return $this->getAll()->lists('name', 'id');
+	}	
+}
