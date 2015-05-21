0f533ddfebb9ac6a2ed31d3605e22da96d8f9e73
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-20 22:36:15 -0430

    Incluyenco modelo sanctios_types

---
+++ app/Soccer/Game/Sanction/SanctionTypeRepository.php
@@ -0,0 +1,27 @@
+<?php namespace soccer\Game\Sanction;
+
+use soccer\Game\Sanction\SanctionType;
+use soccer\Base\BaseRepository;
+use Carbon\Carbon;
+use Datatable;
+use Illuminate\Database\Eloquent\Collection;
+
+
+/**
+* 
+*/
+class SanctionTypeRepository extends BaseRepository
+{
+	
+	function __construct() {
+		$this->columns = [];
+
+		$this->setModel(new SanctionType);
+		$this->setListAllRoute('');
+	}
+
+	public function getAllForSelect()
+	{
+		return $this->getAll()->lists('name', 'id');
+	}	
+}
