bd58580e257cabcea1deb59195c75626f0b4f35b
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-20 22:00:37 -0430

    Actualizando blade

--- app/views/games/add-sanction.blade.php
+++ app/views/games/add-sanction.blade.php
@@ -1,18 +1,52 @@
 <div class="col-md-12">
 	<!-- BOX -->
-	<div  id="add-teams-to-group-form-div-box" class="box border primary col-md-12">
+	<div  id="add-sanction-to-game-form-div-box" class="box border primary col-md-12">
 		<div class="box-title">
-			<h4><i class="fa fa-user"></i><span class="hidden-inline-mobile">Agregar Equipos</span></h4>
+			<h4><i class="fa fa-user"></i><span class="hidden-inline-mobile">Agregar Goles</span></h4>
 		</div>
 		<div class="box-body">
-			{{ Form::open(['route' => ['groups.api.add.team'], 'class' => 'form-horizontal', 'role' => 'form', 'id' => 'add-team-to-group-form']) }}
+			{{ Form::open(['route' => ['games.api.add.sanction'], 'class' => 'form-horizontal', 'role' => 'form', 'id' => 'add-sanction-to-game-form']) }}
 
 			<div class="form-group">
-				{{ Form::label('teams_ids[]','Equipos',['class'=>'col-md-2 control-label']) }}
+				{{ Form::label('observations','Observación',['class'=>'col-sm-2 control-label']) }}
+				<div class="col-sm-6">
+					{{ Form::text('observations',null, ['class' => 'form-control','placeholder'=>'','id' =>'observations-sanction']) }}
+				</div>
+			</div>
+
+			<div class="form-group">
+			{{ Form::label('minute','Minuto',['class'=>'col-sm-2 control-label']) }}
+				<div class="col-sm-6">
+				{{ Form::text('minute',null, ['class' => 'form-control','id' => 'minute-sanction']) }}
+				</div>
+			</div>
+			<div class="form-group">
+				{{ Form::label('second','Segundo',['class'=>'col-sm-2 control-label']) }}
+				<div class="col-sm-6">
+					{{ Form::text('second',null, ['class' => 'form-control','id' => 'second-sanction']) }}
+				</div>
+			</div>
+		
+			<div class="form-group">
+				{{ Form::label('type_id','Tipo de gol',['class'=>'col-md-2 control-label']) }}
 				<div class="col-md-6">
-					{{ Form::select('teams_ids[]',array(),null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoger Equipos...','id'=>'new-teams-for-groups-ids','multiple' => true]) }}
+					{{ Form::select('type_id',[],null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoger Tipo...','id'=>'goal-types-for-sanction-id']) }}
+				</div>
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('team_id','Equipo',['class'=>'col-md-2 control-label']) }}
+				<div class="col-md-6">
+					{{ Form::select('team_id',[],null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoger Equipo...','id'=>'teams-for-sanction-id']) }}
 				</div>
 			</div>	
+
+			<div class="form-group">
+				{{ Form::label('player_id','Jugador',['class'=>'col-md-2 control-label']) }}
+				<div class="col-md-6">
+					{{ Form::select('player_id',[],null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoger Jugadores...','id'=>'player-for-sanction-id']) }}
+				</div>
+			</div>
 			{{ Form::close() }}
 		</div>
 	</div>
