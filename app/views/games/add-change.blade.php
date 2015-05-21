367c004404101d2a6c7d3a581a0e6d464f5663ff
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-21 02:01:22 -0430

    Actualizando blades

--- app/views/games/add-change.blade.php
+++ app/views/games/add-change.blade.php
@@ -1,18 +1,52 @@
 <div class="col-md-12">
 	<!-- BOX -->
-	<div  id="add-teams-to-group-form-div-box" class="box border primary col-md-12">
+	<div  id="add-change-to-game-form-div-box" class="box border primary col-md-12">
 		<div class="box-title">
-			<h4><i class="fa fa-user"></i><span class="hidden-inline-mobile">Agregar Equipos</span></h4>
+			<h4><i class="fa fa-user"></i><span class="hidden-inline-mobile">Agregar Sanción</span></h4>
 		</div>
 		<div class="box-body">
-			{{ Form::open(['route' => ['groups.api.add.team'], 'class' => 'form-horizontal', 'role' => 'form', 'id' => 'add-team-to-group-form']) }}
+			{{ Form::open(['route' => ['games.api.add.change'], 'class' => 'form-horizontal', 'role' => 'form', 'id' => 'add-change-to-game-form']) }}
 
 			<div class="form-group">
-				{{ Form::label('teams_ids[]','Equipos',['class'=>'col-md-2 control-label']) }}
+				{{ Form::label('observations','Observación',['class'=>'col-sm-2 control-label']) }}
+				<div class="col-sm-6">
+					{{ Form::text('observations',null, ['class' => 'form-control','placeholder'=>'','id' =>'observations-change']) }}
+				</div>
+			</div>
+
+			<div class="form-group">
+			{{ Form::label('minute','Minuto',['class'=>'col-sm-2 control-label']) }}
+				<div class="col-sm-6">
+				{{ Form::text('minute',null, ['class' => 'form-control','id' => 'minute-change']) }}
+				</div>
+			</div>
+			<div class="form-group">
+				{{ Form::label('second','Segundo',['class'=>'col-sm-2 control-label']) }}
+				<div class="col-sm-6">
+					{{ Form::text('second',null, ['class' => 'form-control','id' => 'second-change']) }}
+				</div>
+			</div>
+		
+			<div class="form-group">
+				{{ Form::label('team_id','Equipo',['class'=>'col-md-2 control-label']) }}
 				<div class="col-md-6">
-					{{ Form::select('teams_ids[]',array(),null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoger Equipos...','id'=>'new-teams-for-groups-ids','multiple' => true]) }}
+					{{ Form::select('team_id',[],null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoger Equipo...','id'=>'teams-for-game-change-id']) }}
 				</div>
 			</div>	
+
+			<div class="form-group">
+				{{ Form::label('player_out_id','Sacar Jugador',['class'=>'col-md-2 control-label']) }}
+				<div class="col-md-6">
+					{{ Form::select('player_out_id',[],null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoger Jugadores...','id'=>'player-out-for-change-id']) }}
+				</div>
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('player_in_id','Ingresar Jugador',['class'=>'col-md-2 control-label']) }}
+				<div class="col-md-6">
+					{{ Form::select('player_in_id',[],null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoger Jugadores...','id'=>'player-in-for-change-id']) }}
+				</div>
+			</div>
 			{{ Form::close() }}
 		</div>
 	</div>
