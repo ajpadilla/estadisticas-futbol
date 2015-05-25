cb2e18d43b51efb60928405e4c4cdba2b3e86a8f
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-25 11:48:16 -0430

    Agregando rutas para modelo Aligment

--- app/routes.php
+++ app/routes.php
@@ -203,6 +203,13 @@ Route::post('sanciones/api-actualizar', array('as'=>'sanctions.api.update', 'use
 Route::get('cambios/api-ver', array('as'=>'changes.api.show', 'uses'=>'ChangeController@showApi'));
 Route::post('cambios/api-actualizar', array('as'=>'changes.api.update', 'uses'=>'ChangeController@updateApi'));
 
+/*
+********************************* ALIGMENT FOR GAMES ********************************
+*/
+
+Route::get('alineaciones/api-ver', array('as'=>'alignments.api.show', 'uses'=>'ChangeController@showApi'));
+Route::post('alineaciones/api-actualizar', array('as'=>'alignments.api.update', 'uses'=>'ChangeController@updateApi'));
+
 
 /*
 ********************************* SANCTIONS TYPES FOR GAMES ********************************
