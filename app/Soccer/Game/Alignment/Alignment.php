3284d04248f52aef0b2d0aed765e1971e7d56118
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-25 12:52:03 -0430

    Agregando atributo fillable

--- app/Soccer/Game/Alignment/Alignment.php
+++ app/Soccer/Game/Alignment/Alignment.php
@@ -3,6 +3,9 @@
 use soccer\Game\FixtureBaseModel;
 
 class Alignment extends FixtureBaseModel {
+
+    protected $fillable = ['observations','game_id','team_id','player_id','position_id','type_id'];
+
     /*
 	********************* Relations ***********************
     */	
