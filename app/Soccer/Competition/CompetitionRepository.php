3e9152c2c16429f8129a6e025e0da74e78c0edf0
Lenyn Alcántara <nightzpy@gmail.com>
2015-05-27 11:34:08 +0000

    order column para groups tables

--- app/Soccer/Competition/CompetitionRepository.php
+++ app/Soccer/Competition/CompetitionRepository.php
@@ -118,6 +118,7 @@ class CompetitionRepository extends BaseRepository
 		if(!$competition->isClean) {
 			$groupRepository = new GroupRepository;			
 			$orderColumn = [$groupRepository->getColumnCount() - 2, $groupRepository->getColumnCount() - 3, $groupRepository->getColumnCount() - 5, $groupRepository->getColumnCount() - 4];
+			$orderColumn = $groupRepository->getColumnCount() - 2;
 			foreach ($competition->phases as $phase) {
 				if($phase->hasAssociateGroups) {
 					$tables[$phase->id]['name'] = $phase->name;
