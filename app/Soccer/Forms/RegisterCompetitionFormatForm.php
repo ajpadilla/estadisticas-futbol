d48add90bac06f465684ad908ed932d4e2d7d40d
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-29 17:09:38 -0430

    Actualizando validaciones

--- app/Soccer/Forms/RegisterCompetitionFormatForm.php
+++ app/Soccer/Forms/RegisterCompetitionFormatForm.php
@@ -5,13 +5,13 @@ use Laracasts\Validation\FormValidator;
 class RegisterCompetitionFormatForm extends FormValidator{
         protected $rules = [
           'name' => 'required|max:128',
-          'groups' => 'required|numeric', 
-          'clasificated_by_group' =>'required|numeric',
+          'groups' => 'numeric|digits_between:1,6', 
+          'clasificated_by_group' =>'numeric|digits_between:1,6',
           'local_away_game' => 'boolean',
           'local_away_game_final' =>'boolean' ,
           'away_goal' => 'boolean',
-          'teams_by_group' => 'required|numeric',
-          'promotion' => 'required|numeric',
-          'descent' => 'required|numeric',
+          'teams_by_group' => 'numeric|digits_between:1,6',
+          'promotion' => 'numeric|digits_between:1,6',
+          'descent' => 'numeric|digits_between:1,6',
      ];
 }
