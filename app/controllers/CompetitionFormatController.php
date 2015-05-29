11931e3da2431fd501bee185b0d6f49475992546
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-29 16:57:31 -0430

    agregando controlador, para modelo competition_formats

---
+++ app/controllers/CompetitionFormatController.php
@@ -0,0 +1,118 @@
+<?php
+
+use soccer\CompetitionFormats\CompetitionFormatRepository;
+use soccer\Forms\RegisterCompetitionFormatForm;
+use Laracasts\Validation\FormValidationException;
+
+class CompetitionFormatController extends \BaseController {
+
+	protected $repository;
+	protected $registerCompetitionFormatForm;
+
+	public function __construct(CompetitionFormatRepository $repository,
+								RegisterCompetitionFormatForm $registerCompetitionFormatForm) {
+		$this->repository = $repository;
+		$this->registerCompetitionFormatForm = $registerCompetitionFormatForm;
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		if(Request::ajax())
+		{
+			$input = Input::all();
+			try
+			{
+				$this->registerCompetitionFormatForm->validate($input);
+				$competitionFormat = $this->repository->create($input);
+				$this->setSuccess(true);
+				$this->addToResponseArray('competitionFormat', $competitionFormat);
+				$this->addToResponseArray('data', $input);
+				return $this->getResponseArrayJson();
+			}
+			catch (FormValidationException $e)
+			{
+				$this->addToResponseArray('errors', $e->getErrors()->all());
+				return $this->getResponseArrayJson();				
+			}
+		}
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+
+
+}
