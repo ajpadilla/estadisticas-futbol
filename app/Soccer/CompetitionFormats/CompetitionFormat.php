1a5f4fee0594c8563b571eab4132e7bcd3df8d5b
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-29 17:46:35 -0430

    Agreagando Custom Methods

--- app/Soccer/CompetitionFormats/CompetitionFormat.php
+++ app/Soccer/CompetitionFormats/CompetitionFormat.php
@@ -9,4 +9,25 @@ use Carbon\Carbon;
 class CompetitionFormat extends Eloquent {
 	protected $fillable = ['name', 'groups', 'clasificated_by_group' ,'local_away_game','local_away_game_final' ,'away_goal','teams_by_group' ,'promotion' ,'descent'  ,
      ];
+
+	/*
+		********************* Relations ***********************
+    */	
+  	/*
+   		********************* Custom Methods ***********************
+  	*/
+  	
+  	public function getLocalAwayGameAttribute($value)
+    {
+        return ($value ? 'Si' : 'No');
+    }
+    public function getLocalAwayGameFinalAttribute($value)
+    {
+        return ($value ? 'Si' : 'No');
+    }
+    public function getAwayGoalAttribute($value)
+    {
+        return ($value ? 'Si' : 'No');
+    }
+
 }
