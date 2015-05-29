a68a246c354aa8821c8136cfc13045d9d577dad3
Alvaro Padilla <ajpadilla88@gmail.com>
2015-05-29 16:58:39 -0430

    Agregando blades, para modelo competition_formats

---
+++ app/views/competition-formats/new.blade.php
@@ -0,0 +1,7 @@
+@section('title-modal')
+	Crear Formatos De Competencia
+@stop
+
+<div id="new-competition-format-form-div" style="display:none">
+	@include('competition-formats.partials._form')
+</div>
