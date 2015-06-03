cafb46f564ab9e5345e9e200237d176a82ac02dd
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-03 12:52:22 -0430

    Agregando atributo fillable

--- app/Soccer/Game/Goal/GoalType.php
+++ app/Soccer/Game/Goal/GoalType.php
@@ -4,6 +4,8 @@ use Eloquent;
 use Carbon\Carbon;
 
 class GoalType extends Eloquent {
+    protected $fillable = ['name'];
+
     /*
 	********************* Relations ***********************
     */	
