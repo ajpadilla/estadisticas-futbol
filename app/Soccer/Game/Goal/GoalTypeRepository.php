4e5b0f6986e1d0230e8072f8acd7c489af6a0453
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-03 13:18:57 -0430

    Agregando metodos para crear datatable

--- app/Soccer/Game/Goal/GoalTypeRepository.php
+++ app/Soccer/Game/Goal/GoalTypeRepository.php
@@ -14,16 +14,37 @@ class GoalTypeRepository extends BaseRepository
 {
 	
 	function __construct() {
-		$this->columns = [];
+		$this->columns = ['Nombre', 'Acciones'];
 
 		$this->setModel(new GoalType);
-		$this->setListAllRoute('');
+		$this->setListAllRoute('goal-types.api.list');
+	}
+	
+/*
+	*********************** DATATABLE SETTINGS ******************************
+*/			
+
+
+	public function setDefaultActionColumn() {
+
+		$this->addColumnToCollection('Acciones', function($model)
+		{
+			$this->cleanActionColumn();
+			$this->addActionColumn("<a  class='edit-goal-type' href='#edit-goal-type-form' id='edit_goal-type_".$model->id."'>Editar</a><br />");
+			$this->addActionColumn("<a class='delete-goal-type' href='#' id='delete_goal-type_".$model->id."'>Eliminar</a>");
+			return implode(" ", $this->getActionColumn());
+		});
 	}
 
+	public function setBodyTableSettings()
+	{		
+		$this->collection->searchColumns('Nombre');
+		$this->collection->orderColumns('Nombre');
 
-	public function getAllForSelect()
-	{
-		return $this->getAll()->lists('name', 'id');
+		$this->collection->addColumn('Nombre', function($model)
+		{
+			 return $model->name;
+		});
 	}	
-	
+
 }
