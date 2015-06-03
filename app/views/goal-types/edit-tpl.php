227709b6893da94f6b08c46d3b22a8737d58d063
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-03 13:42:19 -0430

    Agregando plnatilla mustache, para editar tipo de gol

--- app/views/goal-types/edit-tpl.php
+++ app/views/goal-types/edit-tpl.php
@@ -1,13 +1,13 @@
-<script id="edit-sanction-type-tpl" type="text/x-handlebars-template">
+<script id="edit-goal-type-tpl" type="text/x-handlebars-template">
 	<div class="box-title">
 		<h4><i class="fa fa-user"></i><span class="hidden-inline-mobile">{{title}}</span></h4>
 	</div>
 	<div class="box-body">
-		<form action="#" method="GET" class='form-horizontal', role='form', id='edit-sanction-type-form'>
+		<form action="#" method="GET" class='form-horizontal', role='form', id='edit-goal-type-form'>
 			<div class="form-group">
 				<label for="name" class = "col-sm-2 control-label" >Nombre</label>
 				<div class="col-sm-6">
-					<input type="text" name="name" class = "form-control" id="name-sanction-type-edit" value="{{name}}" />
+					<input type="text" name="name" class = "form-control" id="name-goal-type-edit" value="{{name}}" />
 				</div>
 			</div>
 		</form>
