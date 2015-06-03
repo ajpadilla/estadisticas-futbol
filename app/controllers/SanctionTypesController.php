30c6e20101c52d02c024f87338d47b63ce42ead8
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-03 11:28:32 -0430

    Agregando metodo store, showApi, updateApi y deleteApi

--- app/controllers/SanctionTypesController.php
+++ app/controllers/SanctionTypesController.php
@@ -1,14 +1,19 @@
 <?php
 
 use soccer\Game\Sanction\SanctionTypeRepository;
+use soccer\Forms\RegisterSanctionTypeForm;
+
 
 class SanctionTypesController extends \BaseController {
 
 
 	protected $repository;
+	protected $registerSanctionTypeForm;
 
-	public function __construct(SanctionTypeRepository $repository) {
+	public function __construct(SanctionTypeRepository $repository,
+		RegisterSanctionTypeForm $registerSanctionTypeForm) {
 		$this->repository = $repository;
+		$this->registerSanctionTypeForm = $registerSanctionTypeForm;
 	}
 
 
@@ -20,7 +25,8 @@ class SanctionTypesController extends \BaseController {
 	 */
 	public function index()
 	{
-		//
+		$table = $this->repository->getAllTable();
+		return View::make('sanction-types.index', compact('table'));
 	}
 
 
@@ -42,8 +48,26 @@ class SanctionTypesController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		if(Request::ajax())
+		{
+			$input = Input::all();
+			try
+			{
+				$this->registerSanctionTypeForm->validate($input);
+				$sanctionType = $this->repository->create($input);
+				$this->setSuccess(true);
+				$this->addToResponseArray('sanctionType', $sanctionType);
+				$this->addToResponseArray('data', $input);
+				return $this->getResponseArrayJson();				
+			}
+			catch (FormValidationException $e)
+			{
+				$this->addToResponseArray('errors', $e->getErrors()->all());
+				return $this->getResponseArrayJson();
+			}
+		}
 	}
+
 
 
 	/**
@@ -113,6 +137,57 @@ class SanctionTypesController extends \BaseController {
 			$this->setSuccess(false);
 			return $this->getResponseArrayJson();
 		}
+	}
+
+
+	public function listApi()
+	{
+		return $this->repository->getDefaultTableForAll();
+	}
+
+	public function showApi()
+	{
+		if (Request::ajax())
+		{
+			if (Input::has('sanctionTypeId'))
+			{
+				$sanctionType = $this->repository->get(Input::get('sanctionTypeId'));
+				$this->setSuccess(($sanctionType ? true : false));
+				$this->addToResponseArray('sanctionType', $sanctionType);
+				return $this->getResponseArrayJson();
+			}else{
+				return $this->getResponseArrayJson();
+			}
+		}
+	}
+
+	public function updateApi()
+	{
+		if(Request::ajax())
+		{
+			$input = Input::all();
+			try
+			{
+				$this->registerSanctionTypeForm->validate($input);
+				$sanctionType = $this->repository->get($input['sanction_type_id']);
+				$sanctionType->update($input);
+				$this->setSuccess(true);
+				$this->addToResponseArray('sanctionType', $sanctionType);
+				return $this->getResponseArrayJson();					
+			}
+			catch (FormValidationException $e)
+			{
+				$this->addToResponseArray('errores', $e->getErrors()->all());
+				return $this->getResponseArrayJson();
+			}
+		}
+	}
+
+	public function destroyApi()
+	{
+		if(Request::ajax())
+			$this->setSuccess($this->repository->delete(Input::get('sanctionTypeId')));
+		return $this->getResponseArrayJson();
 	}
 
 }
