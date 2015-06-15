633f6a49482ccb790b7ccdcf5a81ebbc8efb7716
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-15 18:19:48 -0430

    Agregando blades, para editar por secciones datas de competencia

---
+++ app/views/competitions/partials/_basic-form-edit.blade.php
@@ -0,0 +1,16 @@
+<h4>Información Básica</h4>
+<div class="form-group">
+	{{ Form::label('nombre', 'Nombre', ['class' => 'col-md-2 control-label']) }}	
+	<div class="col-md-6">{{ Form::text('nombre', null, ['class' => 'form-control', 'id'=>'name-competition-edit']) }}</div>
+</div>
+<div class="form-group">
+	{{ Form::label('desde', 'Desde', ['class' => 'col-md-2 control-label']) }}	
+	<div class="col-md-6">{{ Form::text('desde', $competition->From, ['class' => 'form-control datepicker', 'id'=>'from-competition-edit']) }}</div>
+</div>
+<div class="form-group">
+	{{ Form::label('hasta', 'Hasta', ['class' => 'col-md-2 control-label']) }}	
+	<div class="col-md-6">{{ Form::text('hasta', $competition->To, ['class' => 'form-control datepicker', 'id'=>'to-competition-edit']) }}</div>
+</div>
+<div class="form-group">
+	{{ Form::file('imagen', ['class' => 'file-upload']) }}
+</div> 
