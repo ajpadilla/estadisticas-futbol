faf713c64ca83cdf1b1bb1b8838b124708ecd0a5
Lenyn Alcántara <nightzpy@gmail.com>
2015-10-01 07:45:45 +0000

    Listo por completo seeder para goles de partidos de competencia 4 fase de grupos inicial

--- app/database/seeds/GoalsTableSeeder.php
+++ app/database/seeds/GoalsTableSeeder.php
@@ -18,17 +18,23 @@ class GoalsTableSeeder extends DatabaseSeeder{
 		$goalTypes = \soccer\Game\Goal\GoalType::all();
 		foreach ($groups as $group) {
 			$games = $group->games;
+			$teams = [0 => NULL, 1 => NULL];
 			foreach ($games as $game) {
+				var_dump("-----------> " . $game->localTeam->nombre . " VS " . $game->awayTeam->nombre);
 				$goals = [];
-				$teams[] = $game->localTeam;
-				$teams[] = $game->awayTeam;
+				$teams[0] = $game->localTeam;
+				$teams[1] = $game->awayTeam;
 
+				var_dump("--------- Keys: ");
+				print_r(array_keys($teams));
 				foreach ($teams as $k => $team) {
 					$goalsQuantity = $faker->numberBetween(0, 6);
+					var_dump("---- Team ----: ");
 					var_dump($team->nombre . ': ' . $goalsQuantity);
 					$alignments = $game->alignments()->whereTeamId($team->id)->lists('player_id');
 					$teamIdx = intval($k);
 					$vsTeam = $teams[($teamIdx > 0 ? $teamIdx - 1 : $teamIdx + 1)];
+					var_dump("---- VSTeam ----: " .$vsTeam->nombre);
 					$vsAlignments = $game->alignments()->whereTeamId($vsTeam->id)->lists('player_id');
 					for ($i = 0; $i < $goalsQuantity; $i++) {
 						$goal = new \soccer\Game\Goal\Goal;
@@ -38,12 +44,13 @@ class GoalsTableSeeder extends DatabaseSeeder{
 						$goal->team()->associate($team);
 						$alignment = $faker->randomElement($alignments);
 						$assistance = $faker->randomElement($alignments);
+						var_dump("Alineacion: " . count($alignments));
 						if ($goal->type_id == 2) {
-							var_dump($vsAlignments);
+							var_dump("Alineacion contrario: " . count($vsAlignments));
 							$alignment = $faker->randomElement($vsAlignments);
-							var_dump('Jugador: ' . $alignment);
 							$assistance = NULL;
 						}
+						var_dump('Jugador: ' . $alignment);
 						$goal->player_id = $alignment;
 						$goal->assistance_id = $assistance;
 						$goals[] = $goal;
