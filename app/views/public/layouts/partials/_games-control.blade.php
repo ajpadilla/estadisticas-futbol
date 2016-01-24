2c7d4919f38941c87d99f8a0dcd82977ef3b08a9
ajpadilla <ajpadilla88@gmail.com>
2016-01-24 02:54:43 -0430

    Verificando ruta seleccionada para cargar enlaces para ver partidos por fechaç

--- app/views/public/layouts/partials/_games-control.blade.php
+++ app/views/public/layouts/partials/_games-control.blade.php
@@ -1,14 +1,41 @@
 <span class="verdegrande">
-	<div style="float: left;width:225px;color: white;font-size:12px">
-		<br>
-		<a href="ayer" style="font-size: 14px;">Ayer</a>
-	</div>
-	<div style="float: left;width:500px;text-align: center;margin: auto">
-		PARTIDOS DE HOY<br><span style="font-size: 10px;">Los partidos se actualizan automaticamente!</span><br style="clear: both;">
-	</div>
-	<div style="float: left;width:225px;color: white;font-size:12px">
-		<br>
-		<a href="man" style="font-size: 14px;">Mañana</a>
-	</div>
+
+	@if(!empty($showTomorrow) && $showTomorrow)
+		<div style="float: left;width:225px;color: white;font-size:12px">
+			<br>
+			<a href="{{ route('public.home') }}" style="font-size: 14px;">Hoy</a>
+		</div>
+		<div style="float: left;width:500px;text-align: center;margin: auto">
+		PARTIDOS DE MAÑANA<br><span style="font-size: 10px;">Los partidos se actualizan automaticamente!</span><br style="clear: both;">
+		</div>
+	@endif
+	
+	@if(!empty($showToday) && $showToday)
+		<div style="float: left;width:225px;color: white;font-size:12px">
+			<br>
+			<a href="{{ route('gamesforday','yesterday') }}" style="font-size: 14px;">Ayer</a>
+		</div>
+		<div style="float: left;width:500px;text-align: center;margin: auto">
+			PARTIDOS DE HOY<br><span style="font-size: 10px;">Los partidos se actualizan automaticamente!</span><br style="clear: both;">
+		</div>
+		<div style="float: left;width:225px;color: white;font-size:12px">
+			<br>
+			<a href="{{ route('gamesforday','tomorrow') }}" style="font-size: 14px;">Mañana</a>
+		</div>
+	@endif
+	
+	@if(!empty($showYesterday) && $showYesterday)
+		<div style="float: left;width:225px;color: white;font-size:12px">
+			<br>
+		</div>
+		<div style="float: left;width:500px;text-align: center;margin: auto">
+		PARTIDOS DE AYER<br><span style="font-size: 10px;">Los partidos se actualizan automaticamente!</span><br style="clear: both;">
+		</div>
+		<div style="float: left;width:225px;color: white;font-size:12px">
+			<br>
+			<a href="{{ route('public.home') }}" style="font-size: 14px;">Hoy</a>
+		</div>
+	@endif
+
 	<div style="clear: both;"></div>
 </span>
