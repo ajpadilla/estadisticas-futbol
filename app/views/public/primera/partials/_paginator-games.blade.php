81ced6b8df134d93ea3bade16c7b79d81cd0bc5c
ajpadilla <ajpadilla88@gmail.com>
2016-01-29 23:03:43 -0430

    Agregando secciones para ruta primera division argentina

---
+++ app/views/public/primera/partials/_paginator-games.blade.php
@@ -0,0 +1,15 @@
+<span class="verdegrande">
+	<div style="float: left;width:225px;color: white;font-size:12px">
+	Anterior<br>
+	<a href="transicion2014.php" style="font-size: 14px;">Torneo Transición 2014</a>
+	</div>
+	<div style="float: left;width:500px;text-align: center;margin: auto">
+	CAMPEONATO 2015<br style="clear: both;">
+	</div>
+	<div style="clear: both;"></div>
+</span>
+<div style="margin: auto; width:100%;text-align: center;color:white">
+<br>
+Promiedos vuelve en febrero
+<br>
+</div>
---
+++ app/views/public/primera/partials/_positions.blade.php
@@ -0,0 +1,31 @@
+<div id="tablaptsactual">
+<span class="datosequipo" style="display: block; text-align: center"><strong>Posiciones Campeonato 2015</strong> </span>  
+<div id="tabla">
+<table id="posiciones" class="tablesorter3" style="width:100%;text-align: center">
+        <thead>
+        <tr style="background: black; color: white">
+        <th class="header">#</th>
+        <th style="width:140px" class="header">Equipo</th>
+        <th class="pts header" style="background: #408080">Pts<div class="tooltip fixed" style="display: none;">Puntos obtenidos. <br><span class="ordenar">Click para ordenar con este criterio.</span></div></th>
+        <th class="pj header">PJ<div class="tooltip fixed" style="display: none;">Partidos jugados. <br><span class="ordenar">Click para ordenar con este criterio.</span></div></th>
+        <th class="pg header">PG<div class="tooltip fixed" style="display: none;">Partidos ganados. <br><span class="ordenar">Click para ordenar con este criterio.</span></div></th>
+        <th class="pe header">PE<div class="tooltip fixed" style="display: none;">Partidos empatados. <br><span class="ordenar">Click para ordenar con este criterio.</span></div></th>
+        <th class="pp header">PP<div class="tooltip fixed" style="display: none;">Partidos perdidos. <br><span class="ordenar">Click para ordenar con este criterio.</span></div></th>
+        <th class="gf header">GF<div class="tooltip fixed" style="display: none;">Goles a favor. <br><span class="ordenar">Click para ordenar con este criterio.</span></div></th>
+        <th class="gc header">GC<div class="tooltip fixed" style="display: none;">Goles en contra. <br><span class="ordenar">Click para ordenar con este criterio.</span></div></th>
+        <th class="dg header">DIF<div class="tooltip fixed" style="display: none;">Diferencia de Gol. <br><span class="ordenar">Click para ordenar con este criterio.</span></div></th>
+        
+        
+        </tr>
+        </thead>
+        <tbody></tbody>
+        </table>
+</div>
+<div class="infotabla">
+<span style="background: #AAF597;color:black">*El campeón y el subcampeón clasifican a la Copa Libertadores 2016.</span><br>
+<span style="background: #7dbc6d;color:black">*Del 3° al 6° jugarán un reducido por otro cupo a la Copa Libertadores 2016.</span><br>
+<span style="background: #fcf912;color:black">*Del 7° al 18° jugarán una liguilla para la clasificación a la Copa Sudamericana 2016.</span><br>
+Nota: Al estar River Plate clasificado a la Copa Libertadores 2016 como actual campeón, el lugar lo ocupará el siguiente equipo de la tabla.<br>
+Nota: Rosario Central esta entrando a la Copa Libertadores 2016 por la Copa Argentina, el lugar lo ocupará el siguiente equipo de la tabla.
+</div>
+</div>
