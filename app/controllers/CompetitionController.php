05a45f4635c07a767ad52dd913dffea0bd283459
ajpadilla <ajpadilla88@gmail.com>
2016-02-03 09:33:22 -0430

    Actualizando controldaro

--- app/controllers/CompetitionController.php
+++ app/controllers/CompetitionController.php
@@ -116,7 +116,7 @@ class CompetitionController extends \BaseController {
 		$currentPhase = $this->repository->getCurrentPhase($id);
 		$formats = $this->competitionFormatRepository->getAll();
 		$winner = $this->repository->winner($id);
-		//$tableTemplate = 'groups.partials._table-template';
+		$tableTemplate = 'groups.partials._table-template';
 		$scriptTableTemplate = 'partials._script-table-template';
 		return View::make('competitions.show', compact('competition', 'winner', 'countries', 'tables', 'gamesTables', 'tableTemplate', 'scriptTableTemplate', 'gameTypes', 'currentPhase', 'formats'));
 	}
