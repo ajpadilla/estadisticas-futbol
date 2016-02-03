51db50cb09792a86016ee61fde14463b6a72736e
ajpadilla <ajpadilla88@gmail.com>
2016-02-03 09:26:24 -0430

    Agregando campo type en reglas de validacion

--- app/Soccer/Forms/EditCompetitionForm.php
+++ app/Soccer/Forms/EditCompetitionForm.php
@@ -5,6 +5,7 @@ use Laracasts\Validation\FormValidator;
 class EditCompetitionForm extends FormValidator{
         protected $rules = [
           'competition_id' => 'required|exists:competitions,id',
+          'type' => 'required',
           'nombre' => 'required|max:128',
           'desde' => 'required|date_format:Y-m-d', 
           'hasta' =>'required|date_format:Y-m-d',
--- app/Soccer/Forms/RegisterCompetition.php
+++ app/Soccer/Forms/RegisterCompetition.php
@@ -5,6 +5,7 @@ use Laracasts\Validation\FormValidator;
 class RegisterCompetition extends FormValidator{
         protected $rules = [
           'nombre' => 'required|max:128',
+          'type' => 'required',
           'desde' => 'required|date_format:Y-m-d', 
           'hasta' =>'required|date_format:Y-m-d',
           'internacional' => 'boolean',
