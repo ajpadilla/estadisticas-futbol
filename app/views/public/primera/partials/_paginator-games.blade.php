b3937c50fffcf2c74c05bd57fc18fdfb80bda376
ajpadilla <ajpadilla88@gmail.com>
2016-02-03 09:16:49 -0430

    Agregando paginador para equipos

--- app/views/public/primera/partials/_paginator-games.blade.php
+++ app/views/public/primera/partials/_paginator-games.blade.php
@@ -1,15 +1,16 @@
 <span class="verdegrande">
-	<div style="float: left;width:225px;color: white;font-size:12px">
-	Anterior<br>
-	<a href="transicion2014.php" style="font-size: 14px;">Torneo Transición 2014</a>
+	<!--<div style="float: left;width:225px;color: white;font-size:12px">
+		Anterior<br>
+		<a href="transicion2014.php" style="font-size: 14px;">Torneo Transición 2014</a>
+	</div>-->
+	<div>
+		{{ $competitions->links() }}
 	</div>
-	<div style="float: left;width:500px;text-align: center;margin: auto">
-	CAMPEONATO 2015<br style="clear: both;">
-	</div>
+	@foreach($competitions as $competition)
+		<div style="float: left;width:500px;text-align: center;margin: auto">
+			{{ $competition->nombre }}
+			<br style="clear: both;">
+		</div>
+	@endforeach
 	<div style="clear: both;"></div>
 </span>
-<div style="margin: auto; width:100%;text-align: center;color:white">
-<br>
-Promiedos vuelve en febrero
-<br>
-</div>
