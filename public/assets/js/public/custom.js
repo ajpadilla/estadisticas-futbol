ed50929f2c039dfc436d03b0df510d1f076db8c5
ajpadilla <ajpadilla88@gmail.com>
2016-02-04 00:15:22 -0430

    Llenando tablas de posiciones por grupos para competancia

--- public/assets/js/public/custom.js
+++ public/assets/js/public/custom.js
@@ -1,6 +1,7 @@
 var CustomPublicApp = function () {
 	
-	
+	$("#positionsForTeams").tablesorter();
+
 	var teamsFormCompetition = function() 
 	{
 		var competitionId = $('#teamsFormCompetition').attr('data-id');
@@ -15,7 +16,29 @@ var CustomPublicApp = function () {
 			data: data,
 			dataType:'json',
 			success: function(response) {
+				var html;
 				console.log(response);
+				$.each(response.groupsFixtures, function(groupIndex, teams) 
+				{
+					$("#positionsForTeams"+groupIndex).tablesorter();
+					$.each(teams, function(teamIndex, team)
+					{
+						html = "<tr style='background: #d5d5d5'>"+
+									"<td>"+teamIndex+"</td>"+
+									"<td align='left'>"+"<strong>"+team.team.nombre+"</strong>"+"</td>"+
+									"<td>"+team.points+"</td>"+
+									"<td>"+team.gamesPlayed+"</td>"+
+									"<td>"+team.winGames+"</td>"+
+									"<td>"+team.tieGames+"</td>"+
+									"<td>"+team.lostGames+"</td>"+
+									"<td>"+team.scoredGoals+"</td>"+
+									"<td>"+team.againstGoals+"</td>"+
+									"<td>"+team.goalsDiff+"</td>"
+								+"</tr>";
+						$("#positionsForTeams-"+groupIndex+" tbody").append(html);
+						$("#positionsForTeams-"+groupIndex+" tbody").trigger("update");
+					})
+				});
 			},
 			error: function(objeto, quepaso, otroobj){
 				console.log(objeto);
