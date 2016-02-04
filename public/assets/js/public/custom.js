f56c71c56da240b84308ad727d90fbec459dba60
ajpadilla <ajpadilla88@gmail.com>
2016-02-04 16:51:22 -0430

    Agregando metodos gamesForFirtsPhase y gameForPhases para cargar fases para la competencia

--- public/assets/js/public/custom.js
+++ public/assets/js/public/custom.js
@@ -1,6 +1,9 @@
 var CustomPublicApp = function () {
 	
-	$("#positionsForTeams").tablesorter();
+	//console.log($("#firstPhase").attr('data-phase-id'))
+
+	var base_url = window.location.origin;
+	console.log(base_url);
 
 	var groupsFixturesForCompetition = function() 
 	{
@@ -17,7 +20,7 @@ var CustomPublicApp = function () {
 			dataType:'json',
 			success: function(response) {
 				var html;
-				console.log(response);
+				//console.log(response);
 				$.each(response.groupsFixtures, function(groupIndex, teams) 
 				{
 					$("#positionsForTeams"+groupIndex).tablesorter();
@@ -48,10 +51,116 @@ var CustomPublicApp = function () {
 		});
 	};
 
+	var gamesForFirtsPhase = function() 
+	{
+		var url = $('.phasesWithGames').attr('href');
+		var data = {
+			phaseId: $("#firstPhase").attr('data-phase-id')
+		}
+		$.ajax({
+			type: 'GET',
+			url: url,
+			data: data,
+			dataType:'json',
+			success: function(response) {
+					//console.log(response);
+					if (response.success) 
+					{
+						$.each(response.gamesFixtures, function(groupIndex, games) 
+						{
+							$.each(games, function(gameIndex, game)
+							{
+								console.log(game);
+								var tableGames = jQuery('#tableGamesForPhase');
+								var data = {
+									time: game.game.date,
+									imgLocalTeam: game.game.imgLocalTeam,
+									imgAwayTeam: game.game.imgAwayTeam,
+									hour: game.game.time,
+									localTema: game.game.localTeam.nombre,
+									awayTema: game.game.awayTeam.nombre,
+									localGoals: game.game.localGoals,
+									awayGoals: game.game.awayGoals,
+									fixturesLocalGoals: game.game.fixturesLocalGoals ? game.game.fixturesLocalGoals : "",
+									fixturesAwayGoals: game.game.fixturesAwayGoals ? game.game.fixturesAwayGoals : ""
+								};
+								var template = jQuery('#tableGamesForPhase-tpl').html();
+								var html = Mustache.to_html(template, data);
+								tableGames.append(html);
+							});
+						});
+						
+					}
+				},
+				error: function(objeto, quepaso, otroobj){
+					console.log(objeto);
+					console.log(quepaso);
+					console.log(otroobj);
+				}
+			});
+	}
+
+	var gameForPhases = function() 
+	{
+		$(".phasesWithGames").click(function(event) 
+		{
+			event.preventDefault();
+			jQuery('#tableGamesForPhase').html('')
+			var phaseId = $(this).attr('data-phase-id');
+			var url = $(this).attr('href');
+			var data = {
+				phaseId: phaseId
+			}
+			$.ajax({
+				type: 'GET',
+				url: url,
+				data: data,
+				dataType:'json',
+				success: function(response) {
+					//console.log(response);
+					if (response.success) 
+					{
+						$.each(response.gamesFixtures, function(groupIndex, games) 
+						{
+							$.each(games, function(gameIndex, game)
+							{
+								console.log(game);
+								var tableGames = jQuery('#tableGamesForPhase');
+								var data = {
+									time: game.game.date,
+									imgLocalTeam: game.game.imgLocalTeam,
+									imgAwayTeam: game.game.imgAwayTeam,
+									hour: game.game.time,
+									localTema: game.game.localTeam.nombre,
+									awayTema: game.game.awayTeam.nombre,
+									localGoals: game.game.localGoals,
+									awayGoals: game.game.awayGoals,
+									fixturesLocalGoals: game.game.fixturesLocalGoals ? game.game.fixturesLocalGoals : "",
+									fixturesAwayGoals: game.game.fixturesAwayGoals ? game.game.fixturesAwayGoals : ""
+								};
+								var template = jQuery('#tableGamesForPhase-tpl').html();
+								var html = Mustache.to_html(template, data);
+								tableGames.append(html);
+							});
+						});
+						
+					}
+				},
+				error: function(objeto, quepaso, otroobj){
+					console.log(objeto);
+					console.log(quepaso);
+					console.log(otroobj);
+				}
+			});
+		});
+	};
+
 
 	return {
 		init: function () {
 			groupsFixturesForCompetition();
+			gamesForFirtsPhase();
+			gameForPhases();
 		}
 	}
 }();
