651c1c81182614d1444cf3ddf88d068fcb2f2a2c
ajpadilla <ajpadilla88@gmail.com>
2016-02-04 22:05:12 -0430

    agreganso template mustache, para cargar nombre de fase por competencia

---
+++ app/views/public/primera/partials/_name-phases-for-competition-tpl.php
@@ -0,0 +1,6 @@
+<script id="phaseForCompetition-tpl" type="text/x-handlebars-template">
+	<div style="float: left; width:380px; font-size: 14px; color: #c2e213; text-align: center;">
+		{{ competition }}<br>
+		{{ phase }}<br>
+	</div>
+</script>
