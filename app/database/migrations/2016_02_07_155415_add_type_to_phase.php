02a121bf11075de4805bebce37912916f0dbe09f
ajpadilla <ajpadilla88@gmail.com>
2016-02-07 14:55:08 -0430

    Agregando campo para definir tipo de fase para competencias de tipo copa

---
+++ app/database/migrations/2016_02_07_155415_add_type_to_phase.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTypeToPhase extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('phases', function(Blueprint $table)
+		{
+			$table->enum('type',
+				array(
+					'',
+					'fase de grupos',
+					'octavos',
+					'cuartos',
+					'semifinal',
+					'final'
+				))
+			->default(null)
+			->after('name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('phases', function(Blueprint $table)
+		{
+			$table->dropColumn('type');
+		});
+	}
+
+}
