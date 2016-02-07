d9a1135495e10c9051cca82503ea454b5a98a1b3
ajpadilla <ajpadilla88@gmail.com>
2016-02-07 15:23:54 -0430

    Agregando seccion para mostrar informacion de cada copa

---
+++ app/views/public/mundial/partials/_info-cup.blade.php
@@ -0,0 +1,17 @@
+<span class="verdegrande">
+
+	<div style="width: 300px;float: left;padding-top: 10px;">
+
+	</div>
+	<div style="width: 280px;float: left">
+		<img src="images/mundial/brasil2014.png">
+	</div>
+	<div style="width: 300px;float: left;font-size:12px">
+		<br>
+		Partido Inaugural: Jueves 12 de junio 2014<br>
+		Partido Final: Domingo 13 de julio 2014<br>
+		Equipos participantes: 32 selecciones<br>
+	</div>
+
+	<div style="clear: both;"></div>
+</span>
