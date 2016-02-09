07f3d016f0765d46c2bc9b16f974b6b3b6da22f2
ajpadilla <ajpadilla88@gmail.com>
2016-02-08 22:56:48 -0430

    Agregando seccion template para cargar partidos por fases

--- app/views/public/mundial/_mundial.blade.php
+++ app/views/public/mundial/_mundial.blade.php
@@ -12,4 +12,71 @@
 	<div style="clear: both;"></div>
 	<br>
 	@include('public.mundial.partials._final-phase')
+	@include('public.mundial.partials._table-games-phase-tpl')
+@stop
+
+@section('scripts')
+	<script type="text/javascript">
+		$(document).ready(function () 
+		{
+			var gameForPhases = function() 
+			{
+				$(".phasesWithGames").click(function(event) 
+				{
+					event.preventDefault();
+
+					jQuery('#tableGamesForPhase').html('');
+
+					var phaseId = $(this).attr('data-phase-id');
+					var url = $(this).attr('href');
+					var data = {
+						phaseId: phaseId,
+						competitionId : $(this).attr('data-competition-id')
+					};
+					console.log(data);
+					$.ajax({
+						type: 'GET',
+						url: url,
+						data: data,
+						dataType:'json',
+						success: function(response) {
+							console.log(response);
+							if (response.success) 
+							{
+								$.each(response.gamesFixtures, function(groupIndex, games) 
+								{
+									$.each(games, function(gameIndex, game)
+									{
+										//console.log(game);
+										var tableGames = jQuery('#tableGamesForPhase');
+										var data = {
+											time: game.game.date,
+											imgLocalTeam: game.game.imgLocalTeam,
+											imgAwayTeam: game.game.imgAwayTeam,
+											hour: game.game.time,
+											localTema: game.game.localTeam.nombre,
+											awayTema: game.game.awayTeam.nombre,
+											localGoals: game.game.localGoals,
+											awayGoals: game.game.awayGoals,
+											fixturesLocalGoals: game.game.fixturesLocalGoals ? game.game.fixturesLocalGoals : "",
+											fixturesAwayGoals: game.game.fixturesAwayGoals ? game.game.fixturesAwayGoals : ""
+										};
+										var template = jQuery('#tableGamesForPhase-tpl').html();
+										var html = Mustache.to_html(template, data);
+										tableGames.append(html);
+									});
+								});
+							}
+						},
+						error: function(objeto, quepaso, otroobj){
+							console.log(objeto);
+							console.log(quepaso);
+							console.log(otroobj);
+						}
+					});
+				});
+			};
+			gameForPhases();
+		});
+	</script>
 @stop
