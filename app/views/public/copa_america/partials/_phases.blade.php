503a036b6f9d7ee4575e5f08334a37aa57fc44cf
ajpadilla <ajpadilla88@gmail.com>
2016-02-09 12:06:48 -0430

    AGregando datos para cada fase de la copa

--- app/views/public/copa_america/partials/_phases.blade.php
+++ app/views/public/copa_america/partials/_phases.blade.php
@@ -1,98 +1,158 @@
-<div id="infoequipo">FASE FINAL</div>
-<div style="margin-left:100px">
-<div id="fixture">
+@if(!empty($americaCups))
+    @foreach($americaCups as $americaCup)
+    <div id="infoequipo">FASE FINAL</div>
+        <?php $gamesCuartos = $competitionRepository->getGamesForTypePhase('cuartos', $americaCup->from, $americaCup->to);  ?>
+        <?php $gamesSemiFinal = $competitionRepository->getGamesForTypePhase('semifinal', $americaCup->from, $americaCup->to);  ?>
+        <?php $gamesFinal = $competitionRepository->getGamesForTypePhase('final', $americaCup->from, $americaCup->to);  ?>
 
-<div id="columna">
-<span class="grupom">Cuartos</span><br>
-<div id="partidono" style="margin-top:2px">
-</div>
-<div id="partido">
+        <div style="margin-left:100px">
+        <div id="fixture">
+        @if($gamesCuartos)
+        <?php $countGamesCuartos = 0; ?>
+        <div id="columna">
+            <span class="grupom">Cuartos</span><br>
+            <div id="partidono" style="margin-top:2px"></div>
+            @foreach($gamesCuartos as $indexGroup => $groups)
+                @foreach($groups as $indexGames => $games)
+                    @foreach($games as $index => $game)
+                        @if(($countGamesCuartos + 1) <= 2)
+                        <div id="partido">
+                            <span class="fecha">{{ $game['dateObject']->toFormattedDateString() }}-{{ $game['time'] }}-{{ $game['game']->stadium }}</span><br>
+                            <div style="float: left;">
+                                <img src="{{ $game['imgLocalTeam'] }}" width="45px" height="45px">
+                            </div>
+                            <div class="cuadrores3">{{ $game['localGoals'] }} - {{ $game['awayGoals'] }}<br><span style="font-size:12px">(3-2)</span></div>
+                            <div style="float: right;"><img src="{{ $game['imgAwayTeam'] }}" width="45px" height="45px"></div>
+                            <br style="clear: both;">
+                            <div style="float:left"><span style="font-size:10px;margin-left:5px"><strong>{{ $game['localTeam']->nombre }}</strong></span></div>
+                            <div style="float:right"><span style="font-size:10px;margin-right:5px"><strong>{{ $game['awayTeam']->nombre }}</strong></span></div>
+                            <?php $countGamesCuartos++ ?>
+                        </div>
+                        <div id="partidono" style="margin-top:4px"></div>
+                        @endif
+                    @endforeach
+                @endforeach
+            @endforeach
+        </div>
+        @endif
 
-    <span class="fecha">Mie. 24 Junio<br> 20:30 hs</span><br>
-    <div style="float: left;"><img src="http://lh3.googleusercontent.com/-l8VftpmuAVQ/Ul_vWu35KLI/AAAAAAAAAf8/264scdJRGWI/s50/chile.png" width="42px" height="42px"></div><div class="cuadrores3">1 - 0</div><div style="float: right;"><img src="http://lh3.googleusercontent.com/-VKvHgrJ1uhQ/UozHag6QjGI/AAAAAAAAAkM/hYmkygTFst4/s52/uruguay.png" width="42px" height="42px"></div>
-    <br style="clear: both;">
-    <div style="float:left"><span style="font-size:10px;margin-left:5px">Chile</span></div><div style="float:right"><span style="font-size:10px;margin-right:5px">Uruguay</span></div>
-    
-</div>
-<div id="partido" style="margin-top:123px">
+        @if($gamesSemiFinal)
+        <?php $countGamesSemiFinal = 0; ?>
+        <div id="columna">
+            <span class="grupom">Semifinal</span><br>
+            <div id="partidono" style="margin-top:80px"></div>
+            @foreach($gamesSemiFinal as $indexGroup => $groups)
+            @foreach($groups as $indexGames => $games)
+            @foreach($games as $index => $game)
+            @if(($countGamesSemiFinal + 1) <= 1)
+            <div id="partido">
+                <span class="fecha">{{ $game['dateObject']->toFormattedDateString() }}-{{ $game['time'] }}-{{ $game['game']->stadium }}</span><br>
+                <div style="float: left;">
+                    <img src="{{ $game['imgLocalTeam'] }}" width="45px" height="45px">
+                </div>
+                <div class="cuadrores3">{{ $game['localGoals'] }} - {{ $game['awayGoals'] }}<br><span style="font-size:12px">(3-2)</span></div>
+                <div style="float: right;"><img src="{{ $game['imgAwayTeam'] }}" width="45px" height="45px"></div>
+                <br style="clear: both;">
+                <div style="float:left"><span style="font-size:10px;margin-left:5px"><strong>{{ $game['localTeam']->nombre }}</strong></span></div>
+                <div style="float:right"><span style="font-size:10px;margin-right:5px"><strong>{{ $game['awayTeam']->nombre }}</strong></span></div>
+                <?php $countGamesSemiFinal++ ?>
+            </div>
+            @endif
+            @endforeach
+            @endforeach
+            @endforeach
+        </div>
+        @endif
 
-    <span class="fecha">Jue. 25 Junio<br> 20:30 hs</span><br>
-    <div style="float: left;"><img src="http://lh3.googleusercontent.com/-2qUjgH8m4aI/VOd4IFUDXCI/AAAAAAAADl8/sj1M5pw-Tmw/s60/bolivia.png" width="42px" height="42px"></div><div class="cuadrores3">1 - 3</div><div style="float: right;"><img src="http://lh4.googleusercontent.com/-6jsf6conz9g/VOd4IsCPpwI/AAAAAAAADlw/9m_-Efr97RE/s54/peru.png" width="42px" height="42px"></div>
-    <br style="clear: both;">
-    <div style="float:left"><span style="font-size:10px;margin-left:5px">Bolivia</span></div><div style="float:right"><span style="font-size:10px;margin-right:5px">Peru</span></div>
-    
-</div>
-</div>
-<div id="columna">
-<span class="grupom">Semifinal</span><br>
-<div id="partido" style="margin-top:170px">
+        @if($gamesFinal)
+        <?php $countGamesFinal = 0; ?>
+        <div id="columna">
+            <span class="grupom">Final</span><br><br><br>
+            <div id="partidono" style="margin-top:-55px"></div>
+            @foreach($gamesFinal as $indexGroup => $groups)
+            @foreach($groups as $indexGames => $games)
+            @foreach($games as $index => $game)
+            @if(($countGamesFinal + 1) <=1)
+            <div>
+                <img src="http://lh3.googleusercontent.com/-uRf0s42LF84/VOeZ_PjBwnI/AAAAAAAADmo/GKXTZNnBc9Y/w80/copaamerica.png" height="100px"><br>
+                <div id="partido" style="margin-top:15px"> 
+                    <span class="fecha">{{ $game['dateObject']->toFormattedDateString() }}-{{ $game['time'] }}-{{ $game['game']->stadium }}</span><br>
+                    <div style="float: left;">
+                        <img src="{{ $game['imgLocalTeam'] }}" width="45px" height="45px">
+                    </div>
+                    <div class="cuadrores3">{{ $game['localGoals'] }} - {{ $game['awayGoals'] }}<br><span style="font-size:12px">(3-2)</span></div>
+                    <div style="float: right;"><img src="{{ $game['imgAwayTeam'] }}" width="45px" height="45px"></div>
+                    <br style="clear: both;">
+                    <div style="float:left"><span style="font-size:10px;margin-left:5px"><strong>{{ $game['localTeam']->nombre }}</strong></span></div>
+                    <div style="float:right"><span style="font-size:10px;margin-right:5px"><strong>{{ $game['awayTeam']->nombre }}</strong></span></div>
+                    <?php $countGamesFinal++ ?>
+                </div>
+            </div>
+            @endif
+            @endforeach
+            @endforeach
+            @endforeach
+        </div>
+        @endif
 
-    <span class="fecha">Lun. 29 Junio<br> 20:30 hs</span><br>
-    <div style="float: left;"><img src="http://lh3.googleusercontent.com/-l8VftpmuAVQ/Ul_vWu35KLI/AAAAAAAAAf8/264scdJRGWI/s50/chile.png" width="42px" height="42px"></div><div class="cuadrores3">2 - 1</div><div style="float: right;"><img src="http://lh4.googleusercontent.com/-6jsf6conz9g/VOd4IsCPpwI/AAAAAAAADlw/9m_-Efr97RE/s54/peru.png" width="42px" height="42px"></div>
-    <br style="clear: both;">
-    <div style="float:left"><span style="font-size:10px;margin-left:5px">Chile</span></div><div style="float:right"><span style="font-size:10px;margin-right:5px">Peru</span></div>
-    
-</div>
+        @if($gamesSemiFinal)
+        <?php $countGamesSemiFinal = 0; ?>
+        <div id="columna">
+            <span class="grupom">Semifinal</span><br>
+            <div id="partidono" style="margin-top:80px"></div>
+            @foreach($gamesSemiFinal as $indexGroup => $groups)
+            @foreach($groups as $indexGames => $games)
+            @foreach($games as $index => $game)
+            @if(($countGamesSemiFinal + 1) > 1)
+            <div id="partido">
+                <span class="fecha">{{ $game['dateObject']->toFormattedDateString() }}-{{ $game['time'] }}-{{ $game['game']->stadium }}</span><br>
+                <div style="float: left;">
+                    <img src="{{ $game['imgLocalTeam'] }}" width="45px" height="45px">
+                </div>
+                <div class="cuadrores3">{{ $game['localGoals'] }} - {{ $game['awayGoals'] }}<br><span style="font-size:12px">(3-2)</span></div>
+                <div style="float: right;"><img src="{{ $game['imgAwayTeam'] }}" width="45px" height="45px"></div>
+                <br style="clear: both;">
+                <div style="float:left"><span style="font-size:10px;margin-left:5px"><strong>{{ $game['localTeam']->nombre }}</strong></span></div>
+                <div style="float:right"><span style="font-size:10px;margin-right:5px"><strong>{{ $game['awayTeam']->nombre }}</strong></span></div>
+            </div>
+            @endif
+            <?php $countGamesSemiFinal++ ?>
+            @endforeach
+            @endforeach
+            @endforeach
+        </div>
+        @endif
+        
+        @if($gamesCuartos)
+        <?php $countGamesCuartos = 0; ?>
+        <div id="columna">
+            <span class="grupom">Cuartos</span><br>
+            <div id="partidono" style="margin-top:2px"></div>
+            @foreach($gamesCuartos as $indexGroup => $groups)
+            @foreach($groups as $indexGames => $games)
+            @foreach($games as $index => $game)
+            @if(($countGamesCuartos + 1) > 2)
+            <div id="partido">
+                <span class="fecha">{{ $game['dateObject']->toFormattedDateString() }}-{{ $game['time'] }}-{{ $game['game']->stadium }}</span><br>
+                <div style="float: left;">
+                    <img src="{{ $game['imgLocalTeam'] }}" width="45px" height="45px">
+                </div>
+                <div class="cuadrores3">{{ $game['localGoals'] }} - {{ $game['awayGoals'] }}<br><span style="font-size:12px">(3-2)</span></div>
+                <div style="float: right;"><img src="{{ $game['imgAwayTeam'] }}" width="45px" height="45px"></div>
+                <br style="clear: both;">
+                <div style="float:left"><span style="font-size:10px;margin-left:5px"><strong>{{ $game['localTeam']->nombre }}</strong></span></div>
+                <div style="float:right"><span style="font-size:10px;margin-right:5px"><strong>{{ $game['awayTeam']->nombre }}</strong></span></div>
+            </div>
+            <div id="partidono" style="margin-top:4px"></div>
+            @endif
+            <?php $countGamesCuartos++ ?>
+            @endforeach
+            @endforeach
+            @endforeach
+        </div>
+        @endif
 
-</div>
-<div id="columna">
-<span class="grupom">FINAL</span><br>
-<br><br>
-<div>
-<img src="http://lh3.googleusercontent.com/-uRf0s42LF84/VOeZ_PjBwnI/AAAAAAAADmo/GKXTZNnBc9Y/w80/copaamerica.png">
-<br>
-<div id="partido" style="margin-top:15px">
-
-    <span class="fecha">Sab. 04 Julio<br> 17:00 hs</span><br>
-    <div style="float: left;"><img src="http://lh3.googleusercontent.com/-l8VftpmuAVQ/Ul_vWu35KLI/AAAAAAAAAf8/264scdJRGWI/s50/chile.png" width="42px" height="42px"></div><div class="cuadrores3">0 - 0<br><span style="font-size:12px">(4-1)</span></div><div style="float: right;"><img src="http://lh3.googleusercontent.com/-arxPZXZ2L7A/Ul_vVBs7hbI/AAAAAAAAAeo/3gRnwOVIZ94/s64/argentina.png" width="42px" height="42px"></div>
-    <br style="clear: both;">
-    <div style="float:left"><span style="font-size:10px;margin-left:5px">Chile</span></div><div style="float:right"><span style="font-size:10px;margin-right:5px">Argentina</span></div>
-    
-</div>
-<br><br>
-<span class="grupom">3°Puesto</span><br>
-<div id="partido" style="margin-top:15px">
-
-    <span class="fecha">Vie. 03 Julio<br> 20:30 hs</span><br>
-    <div style="float: left;"><img src="http://lh4.googleusercontent.com/-6jsf6conz9g/VOd4IsCPpwI/AAAAAAAADlw/9m_-Efr97RE/s54/peru.png" width="42px" height="42px"></div><div class="cuadrores3">2 - 0</div><div style="float: right;"><img src="http://lh5.googleusercontent.com/-p1Cih5Jirxg/VOd4ITfABZI/AAAAAAAADls/xxpNKX4GK5w/s52/paraguay.png" width="42px" height="42px"></div>
-    <br style="clear: both;">
-    <div style="float:left"><span style="font-size:10px;margin-left:5px">Peru</span></div><div style="float:right"><span style="font-size:10px;margin-right:5px">Paraguay</span></div>
-    
-</div>
-</div>
-
-</div>
-<div id="columna">
-<span class="grupom">Semifinal</span><br>
-<div id="partido" style="margin-top:170px">
-
-    <span class="fecha">Mar. 30 Junio<br> 20:30 hs</span><br>
-    <div style="float: left;"><img src="http://lh3.googleusercontent.com/-arxPZXZ2L7A/Ul_vVBs7hbI/AAAAAAAAAeo/3gRnwOVIZ94/s64/argentina.png" width="42px" height="42px"></div><div class="cuadrores3">6 - 1</div><div style="float: right;"><img src="http://lh5.googleusercontent.com/-p1Cih5Jirxg/VOd4ITfABZI/AAAAAAAADls/xxpNKX4GK5w/s52/paraguay.png" width="42px" height="42px"></div>
-    <br style="clear: both;">
-    <div style="float:left"><span style="font-size:10px;margin-left:5px">Argentina</span></div><div style="float:right"><span style="font-size:10px;margin-right:5px">Paraguay</span></div>
-    
-</div>
-</div>
-<div id="columna">
-<span class="grupom">Cuartos</span><br>
-<div id="partidono" style="margin-top:2px">
-</div>
-<div id="partido">
-
-    <span class="fecha">Vie. 26 Junio<br> 20:30 hs</span><br>
-    <div style="float: left;"><img src="http://lh3.googleusercontent.com/-arxPZXZ2L7A/Ul_vVBs7hbI/AAAAAAAAAeo/3gRnwOVIZ94/s64/argentina.png" width="42px" height="42px"></div><div class="cuadrores3">0 - 0<br><span style="font-size:12px">(5-4)</span></div><div style="float: right;"><img src="http://lh5.googleusercontent.com/-dolnTiZ8Nhw/Ul_vW1--EtI/AAAAAAAAAfQ/EFjCJO8xLNI/s50/colombia.png" width="42px" height="42px"></div>
-    <br style="clear: both;">
-    <div style="float:left"><span style="font-size:10px;margin-left:5px">Argentina</span></div><div style="float:right"><span style="font-size:10px;margin-right:5px">Colombia</span></div>
-    
-</div>
-<div id="partido" style="margin-top:123px">
-
-    <span class="fecha">Sab. 27 Junio<br> 18:30 hs</span><br>
-    <div style="float: left;"><img src="http://lh5.googleusercontent.com/-C318rUE6W3U/Ul_vWNwto4I/AAAAAAAAAe8/B7Rx-Dj5B_Y/s64/brasil.png" width="42px" height="42px"></div><div class="cuadrores3">1 - 1<br><span style="font-size:12px">(3-4)</span></div><div style="float: right;"><img src="http://lh5.googleusercontent.com/-p1Cih5Jirxg/VOd4ITfABZI/AAAAAAAADls/xxpNKX4GK5w/s52/paraguay.png" width="42px" height="42px"></div>
-    <br style="clear: both;">
-    <div style="float:left"><span style="font-size:10px;margin-left:5px">Brasil</span></div><div style="float:right"><span style="font-size:10px;margin-right:5px">Paraguay</span></div>
-    
-</div>
-</div>
-
-</div></div>
+        </div>
+    </div>
+    @endforeach
+@endif
