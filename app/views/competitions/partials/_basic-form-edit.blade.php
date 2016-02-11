4c854fb7fe322b544ef633b1c8f6a73e05a647c6
ajpadilla <ajpadilla88@gmail.com>
2016-02-10 21:48:22 -0430

    Modificando valores para select type competition

--- app/views/competitions/partials/_basic-form-edit.blade.php
+++ app/views/competitions/partials/_basic-form-edit.blade.php
@@ -7,47 +7,22 @@
 	{{ Form::label('type','Tipo',['class'=>'col-sm-2 control-label']) }}
 	<div class="col-sm-6">
 		{{ Form::select('type',
-			[   'primera' => 'primera',
-			'liguilla primera' => 'liguilla primera',
-			'desempate descenso primera' => 'desempate descenso primera',
-
-			'b nacional' => 'b nacional',
-			'desempate ascenso b nacional' => 'desempate ascenso b nacional',
-			'desempate descenso b nacional' => 'desempate descenso b nacional',
-			'reducido b nacional' => 'reducido b nacional',
-
-			'b metro' => 'b metro',
-			'desempate ascenso b metro' => 'desempate ascenso b metro',
-			'desempate descenso b metro' => 'desempate descenso b metro',
-			'reducido b metro' => 'reducido b metro',
-
-			'federal a' => 'federal a',
-			'desempate ascenso federal a' => 'desempate ascenso federal a',
-			'desempate descenso federal a' => 'desempate descenso federal a',
-			'reducido federal a' => 'reducido federal a',
-
-			'primera c' => 'primera c',
-			'desempate ascenso primera c' => 'desempate ascenso primera c',
-			'desempate descenso primera c' => 'desempate descenso primera c',
-			'reducido primera c' => 'reducido primera c',
-
-			'primera d' => 'primera d',
-			'desempate ascenso primera d' => 'desempate ascenso primera d',
-			'desempate descenso primera d' => 'desempate descenso primera d',
-			'reducido primera d' => 'reducido primera d',
-
-			'federal b' => 'federal b',
-			'desempate ascenso federal b' => 'desempate ascenso federal b',
-			'desempate descenso federal b' => 'desempate descenso federal b',
-			'reducido federal b' => 'reducido federal b',
-			
-			'copa mundial' => 'copa mundial',
-			'copa america' => 'copa america',
-			'copa libertadores' => 'copa libertadores',
-			'champion L' => 'champion L',
-			'copa argentina' => 'copa argentina',
-			'sudamericana' => 'sudamericana',
-			'mundial de clubes' => 'mundial de clubes'
+			[   
+				'primera' => 'primera',
+				'b nacional' => 'b nacional',
+				'b metro' => 'b metro',
+				'federal a' => 'federal a',
+				'primera c' => 'primera c',
+				'primera d' => 'primera d',
+				'federal b' => 'federal b',
+				'copa mundial' => 'copa mundial',
+				'eliminatorias copa mundial' => 'eliminatorias copa mundial',
+				'copa america' => 'copa america',
+				'copa libertadores' => 'copa libertadores',
+				'champion L' => 'champion L',
+				'copa argentina' => 'copa argentina',
+				'sudamericana' => 'sudamericana',
+				'mundial de clubes' => 'mundial de clubes'
 			]
 			,$competition->type,['class' => 'form-control chosen-select','data-placeholder' => 'Escoge Tipo...','id'=> 'type_competition']) }}
 		</div>
