ab19fd4d1fb8b344ea31c5b5a64c7f78d955ea80
ajpadilla <ajpadilla88@gmail.com>
2016-02-12 12:25:46 -0430

    Verificando si existen fases para la competencia

--- app/views/public/primera/partials/_shields.blade.php
+++ app/views/public/primera/partials/_shields.blade.php
@@ -1,6 +1,22 @@
-<div class="expand-down" align="center" style="width: 920px;">
-<span class="datosequipo2">Click en el escudo para ver info <span class="elequipo"></span></span><br>
-<ul>
-	<li><a href="aldosivi.html"><img src="images/s30/aldosivi.png" onmouseover="equipo('Aldosivi (Mar del Plata)');" onmouseout="noequipo();"></a></li>
-</ul>
+<div class="expand-down-liga" align="center" style="width: 900px;">
+	<!--<span class="datosequipo2">Click en el escudo para ver info <span class="elequipo"></span></span><br>-->
+	@foreach($competitions as $competition)
+		<?php 
+			if(!empty($competition->hasPhases))
+				$firstPhase = $competition->phases->first();
+				if(!empty($firstPhase))
+					$groupsForfirstPhase = $firstPhase->groups;
+		?>
+		<ul>
+			@if(!empty($groupsForfirstPhase))
+				@foreach($groupsForfirstPhase as $group)
+					@foreach($group->teams as $team)
+					<li>
+						<a href=""><img src="{{ $team->escudo->url('thumb') }}"></a>
+					</li>
+					@endforeach
+				@endforeach
+			@endif
+		</ul>
+	@endforeach
 </div>
