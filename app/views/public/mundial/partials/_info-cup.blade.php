23ab2c022451153dce605109e46eb71e6fe78ccb
ajpadilla <ajpadilla88@gmail.com>
2016-02-14 14:51:09 -0430

    Modificando carga de datos para el blade

--- app/views/public/mundial/partials/_info-cup.blade.php
+++ app/views/public/mundial/partials/_info-cup.blade.php
@@ -1,8 +1,16 @@
 @if(!empty($cups))
 <span class="verdegrande">
-	<div style="padding-top: 10px;">
+	<!--<div style="float: left;width:225px;color: white;font-size:12px">
+		Anterior<br>
+		<a href="transicion2014.php" style="font-size: 14px;">Torneo Transición 2014</a>
+	</div>-->
+	@foreach($cups as $cup)
 		{{ $cups->links() }}
-	</div>
+	@endforeach
+	<div style="clear: both;"></div>
+
+</span>
+<span class="verdegrande">
 		@foreach($cups as $cup)
 		<div>
 			<div style="width: 280px;float: left">
@@ -15,6 +23,7 @@
 			Partido Final:  {{ $cup->formatTo }}<br>
 			Equipos participantes: 32 selecciones<br>
 		</div>
+		<a id="currentCompetitionId" href="" class="phasesWithGames" data-competition-id = "{{ $cup->id }}"></a>
 		@endforeach
 	</div>
 	<div style="clear: both;"></div>
