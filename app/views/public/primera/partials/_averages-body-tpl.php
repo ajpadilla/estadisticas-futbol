6bb8899cc5f27dfe42f2918b821c974fc772aecb
ajpadilla <ajpadilla88@gmail.com>
2016-02-14 14:53:15 -0430

    Agregando templates

---
+++ app/views/public/primera/partials/_averages-body-tpl.php
@@ -0,0 +1,6 @@
+<script id="averages-tpl" type="text/x-handlebars-template">
+	<table id="tablapromactual" class="tablesorter" style="width: 100%;text-align: center">
+		<tbody>
+		</tbody>
+	</table>
+</script>
