ad12ab5461ac1681fe884a4efdebc469156631f4
ajpadilla <ajpadilla88@gmail.com>
2016-02-15 14:46:17 -0430

    Modificando metodo gamesForChampionsCup

--- app/controllers/PublicController.php
+++ app/controllers/PublicController.php
@@ -362,14 +362,63 @@ class PublicController extends \BaseController {
 
 	public function gamesForChampionsCup()
 	{
+		$currentCup = null;
+		$gamesForPhases = [];
+		$scoredGoals = null;
+		$tablePosTeams = null;
+		$firstPhase = null;
+		$winner = null;
+		$gamesOctavos = null;
+		$gamesCuartos = null;
+		$gamesSemiFinal = null;
+		$gamesFinal = null;
+		
 		$championsCups = $this->competitionRepository
 		->getModel()
 		->whereType('champion L')
 		->orderBy('desde', 'desc')
 		->orderBy('hasta', 'desc')
 		->paginate(1);
-		$competitionRepository =  $this->competitionRepository;
-	 	return View::make('public.champions._champions', compact('championsCups','competitionRepository'));
+
+		if(count($championsCups) >0 )
+			foreach ($championsCups as $cup)
+				$currentCup = $this->competitionRepository->get($cup->id);
+
+		if(!empty($currentCup))
+		{
+			foreach ($currentCup->phases as $phase) {
+				$gamesForPhases[$phase->id] = $this->competitionRepository->getGamesForPhase($phase->id);
+			}
+
+			if($currentCup->hasPhases)
+			{
+				$firstPhase = $currentCup->phases->first();
+				if(!empty($firstPhase) && !empty($firstPhase->hasGroups)){
+					$gamesForGroups = $this->competitionRepository->getGamesForPhase($firstPhase->id);
+					foreach ($firstPhase->groups as $group) {
+						$tablePosTeams = $this->competitionRepository->getPostTeamsForGruop($group->id);
+					}
+				}
+			}
+			$winner= $this->competitionRepository->winner($currentCup->id);
+			$gamesOctavos = $this->competitionRepository->getGamesForTypePhase('octavos', $currentCup->from, $currentCup->to);
+			$gamesCuartos = $this->competitionRepository->getGamesForTypePhase('cuartos', $currentCup->from, $currentCup->to);
+			$gamesSemiFinal = $this->competitionRepository->getGamesForTypePhase('semifinal', $currentCup->from, $currentCup->to);
+			$gamesFinal = $this->competitionRepository->getGamesForTypePhase('final', $currentCup->from, $currentCup->to);
+		}
+	 	return View::make('public.champions._champions', compact(
+	 													'championsCups',
+	 													'currentCup',
+	 													'gamesForPhases',
+	 													'tablePosTeams',
+	 													'gamesForGroups',
+	 													'winner',
+	 													'gamesOctavos',
+	 													'gamesCuartos',
+	 													'gamesSemiFinal',
+	 													'gamesFinal'
+	 												)
+	 	);
 	}
 
 	public function gamesForArgentinaCup()
