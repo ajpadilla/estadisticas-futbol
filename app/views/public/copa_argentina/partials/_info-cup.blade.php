8ad8c77570d855719869815bb167170b781f5e2d
ajpadilla <ajpadilla88@gmail.com>
2016-02-15 15:02:07 -0430

    Modificando blades

--- app/views/public/copa_argentina/partials/_info-cup.blade.php
+++ app/views/public/copa_argentina/partials/_info-cup.blade.php
@@ -4,11 +4,10 @@
 </div>
 @foreach($argentinaCups as $argentinaCup)
 <span class="verdegrande">
-	COPA ARGENTINA {{ $argentinaCup->year }}
+	COPA ARGENTINA  {{$argentinaCup->year}}
 </span>
 <div style="clear: both;"></div>
 <br/>
-<?php $winner = $competitionRepository->winner($argentinaCup->id) ?>
 @if(!empty($winner))
 <div style="width: 980px;height: auto;background: #17573d;font-size:12px;color:white;text-align: center;">
 	<br>
