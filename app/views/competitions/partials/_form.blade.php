22f064d534d389e779d02e17d9753162b4bf381a
ajpadilla <ajpadilla88@gmail.com>
2016-02-17 13:49:22 -0430

    Actualizando lista de tipos de competencias

--- app/views/competitions/partials/_form.blade.php
+++ app/views/competitions/partials/_form.blade.php
@@ -28,41 +28,41 @@
 						{{ Form::label('type','Tipo',['class'=>'col-sm-2 control-label']) }}
 						<div class="col-sm-6">
 							{{ Form::select('type',
-								[   'primera',
-									'desempate descenso primera'
-									'liguilla'
-									'b nacional',
-									'desempate ascenso b nacional'
-									'desempate descenso b nacional'
-									'reducido b nacional'
-									'b metro',
-									'desempate ascenso b metro'
-									'desempate descenso b metro'
-									'reducido b metro'
-									'federal a',
-									'desempate ascenso federal a'
-									'desempate descenso federal a'
-									'reducido federal a'
-									'primera c',
-									'desempate ascenso primera c'
-									'desempate descenso primera c'
-									'reducido primera c'
-									'primera d',
-									'desempate ascenso primera d'
-									'desempate descenso primera d'
-									'reducido primera d'
-									'federal b',
-									'desempate ascenso federal b'
-									'desempate descenso federal b'
-									'reducido federal b'
-									'copa mundial',
-									'eliminatorias copa mundial',
-									'copa america',
-									'copa libertadores',
-									'champion L',
-									'copa argentina',
-									'sudamericana',
-									'mundial de clubes'
+								[   'primera' => 'primera',
+									'desempate descenso primera' => 'desempate descenso primera',
+									'liguilla' => 'liguilla',
+									'b nacional' => 'b nacional',
+									'desempate ascenso b nacional' => 'desempate ascenso b nacional',
+									'desempate descenso b nacional' => 'desempate descenso b nacional',
+									'reducido b nacional' => 'reducido b nacional',
+									'b metro' => 'b metro',
+									'desempate ascenso b metro' => 'desempate ascenso b metro',
+									'desempate descenso b metro' => 'desempate descenso b metro',
+									'reducido b metro' => 'reducido b metro',
+									'federal a' => 'federal a',
+									'desempate ascenso federal a' => 'desempate ascenso federal a',
+									'desempate descenso federal a' => 'desempate descenso federal a',
+									'reducido federal a' => 'reducido federal a',
+									'primera c' => 'primera c',
+									'desempate ascenso primera c' => 'desempate ascenso primera c',
+									'desempate descenso primera c' => 'desempate descenso primera c',
+									'reducido primera c'  => 'reducido primera c',
+									'primera d'  => 'primera d',
+									'desempate ascenso primera d'  => 'desempate ascenso primera d',
+									'desempate descenso primera d'  =>  'desempate descenso primera d',
+									'reducido primera d'  => 'reducido primera d',
+									'federal b'  => 'federal b',
+									'desempate ascenso federal b' =>  'desempate ascenso federal b',
+									'desempate descenso federal b' =>  'desempate descenso federal b',
+									'reducido federal b'  => 'reducido federal b',
+									'copa mundial' =>  'copa mundial',
+									'eliminatorias copa mundial'  => 'eliminatorias copa mundial',
+									'copa america'  => 'copa america',
+									'copa libertadores' =>  'copa libertadores',
+									'champion L'  => 'champion L',
+									'copa argentina'  => 'copa argentina',
+									'sudamericana'  => 'sudamericana',
+									'mundial de clubes' => 'mundial de clubes'
 								]
 							,null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoge Tipo...','id'=> 'type_competition']) }}
 						</div>
@@ -79,6 +79,12 @@
 						{{ Form::label('competition_format_id','Formato de competencía',['class'=>'col-sm-2 control-label']) }}
 						<div class="col-sm-6">
 							{{ Form::select('competition_format_id',array(),null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoge formato...','id'=> 'competition_format_id']) }}
+						</div>
+					</div>
+					<div class="form-group">
+						{{ Form::label('previous_id','Competencia previa',['class'=>'col-sm-2 control-label']) }}
+						<div class="col-sm-6">
+						{{ Form::select('previous_id',[],null,['class' => 'form-control chosen-select','data-placeholder' => 'Escoge competencia...','id'=>'competition_previous_id']) }}
 						</div>
 					</div>
 					<div class="form-group">
