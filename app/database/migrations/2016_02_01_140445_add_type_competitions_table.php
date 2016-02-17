ab993024a4c0f2ee7804767ad0db42133cf74169
ajpadilla <ajpadilla88@gmail.com>
2016-02-17 13:43:42 -0430

    Actualizando valores a incluir

--- app/database/migrations/2016_02_01_140445_add_type_competitions_table.php
+++ app/database/migrations/2016_02_01_140445_add_type_competitions_table.php
@@ -17,32 +17,32 @@ class AddTypeCompetitionsTable extends Migration {
 			$table->enum('type', 
 				array(
 						'primera',
-						'desempate descenso primera'
-						'liguilla'
+						'desempate descenso primera',
+						'liguilla',
 						'b nacional',
-						'desempate ascenso b nacional'
-						'desempate descenso b nacional'
-						'reducido b nacional'
+						'desempate ascenso b nacional',
+						'desempate descenso b nacional',
+						'reducido b nacional',
 						'b metro',
-						'desempate ascenso b metro'
-						'desempate descenso b metro'
-						'reducido b metro'
+						'desempate ascenso b metro',
+						'desempate descenso b metro',
+						'reducido b metro',
 						'federal a',
-						'desempate ascenso federal a'
-						'desempate descenso federal a'
-						'reducido federal a'
+						'desempate ascenso federal a',
+						'desempate descenso federal a',
+						'reducido federal a',
 						'primera c',
-						'desempate ascenso primera c'
-						'desempate descenso primera c'
-						'reducido primera c'
+						'desempate ascenso primera c',
+						'desempate descenso primera c',
+						'reducido primera c',
 						'primera d',
-						'desempate ascenso primera d'
-						'desempate descenso primera d'
-						'reducido primera d'
+						'desempate ascenso primera d',
+						'desempate descenso primera d',
+						'reducido primera d',
 						'federal b',
-						'desempate ascenso federal b'
-						'desempate descenso federal b'
-						'reducido federal b'
+						'desempate ascenso federal b',
+						'desempate descenso federal b',
+						'reducido federal b',
 						'copa mundial',
 						'eliminatorias copa mundial',
 						'copa america',
