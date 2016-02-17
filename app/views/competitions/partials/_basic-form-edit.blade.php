3bb5ea86ef0450fd3cf0db847c579d2eeb1c71c7
ajpadilla <ajpadilla88@gmail.com>
2016-02-17 13:48:46 -0430

    Actualizando lista de tipos de competencias

--- app/views/competitions/partials/_basic-form-edit.blade.php
+++ app/views/competitions/partials/_basic-form-edit.blade.php
@@ -8,41 +8,41 @@
 	<div class="col-sm-6">
 		{{ Form::select('type',
 			[   
-				'primera',
-				'desempate descenso primera'
-				'liguilla'
-				'b nacional',
-				'desempate ascenso b nacional'
-				'desempate descenso b nacional'
-				'reducido b nacional'
-				'b metro',
-				'desempate ascenso b metro'
-				'desempate descenso b metro'
-				'reducido b metro'
-				'federal a',
-				'desempate ascenso federal a'
-				'desempate descenso federal a'
-				'reducido federal a'
-				'primera c',
-				'desempate ascenso primera c'
-				'desempate descenso primera c'
-				'reducido primera c'
-				'primera d',
-				'desempate ascenso primera d'
-				'desempate descenso primera d'
-				'reducido primera d'
-				'federal b',
-				'desempate ascenso federal b'
-				'desempate descenso federal b'
-				'reducido federal b'
-				'copa mundial',
-				'eliminatorias copa mundial',
-				'copa america',
-				'copa libertadores',
-				'champion L',
-				'copa argentina',
-				'sudamericana',
-				'mundial de clubes'
+						'primera',
+						'desempate descenso primera',
+						'liguilla',
+						'b nacional',
+						'desempate ascenso b nacional',
+						'desempate descenso b nacional',
+						'reducido b nacional',
+						'b metro',
+						'desempate ascenso b metro',
+						'desempate descenso b metro',
+						'reducido b metro',
+						'federal a',
+						'desempate ascenso federal a',
+						'desempate descenso federal a',
+						'reducido federal a',
+						'primera c',
+						'desempate ascenso primera c',
+						'desempate descenso primera c',
+						'reducido primera c',
+						'primera d',
+						'desempate ascenso primera d',
+						'desempate descenso primera d',
+						'reducido primera d',
+						'federal b',
+						'desempate ascenso federal b',
+						'desempate descenso federal b',
+						'reducido federal b',
+						'copa mundial',
+						'eliminatorias copa mundial',
+						'copa america',
+						'copa libertadores',
+						'champion L',
+						'copa argentina',
+						'sudamericana',
+						'mundial de clubes'
 			]
 			,$competition->type,['class' => 'form-control chosen-select','data-placeholder' => 'Escoge Tipo...','id'=> 'type_competition']) }}
 		</div>
