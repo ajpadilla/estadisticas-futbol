0c110831d200663b4a8b1313443ac655cd0601b7
ajpadilla <ajpadilla88@gmail.com>
2016-02-18 11:21:18 -0430

    Agregando ruta a blades b_nacional

--- app/views/public/b_nacional/_b_nacional.blade.php
+++ app/views/public/b_nacional/_b_nacional.blade.php
@@ -12,38 +12,38 @@
 	<div style="clear: both;"></div>
 	<br/>
 	<div style="clear: both;"></div>
-	@include('public.primera.partials._paginator-games')
+	@include('public.b_nacional.partials._paginator-games')
 	<div style="clear: both;"></div>
-	@include('public.primera.partials._shields')
+	@include('public.b_nacional.partials._shields')
 	<div style="clear: both;"></div>
-	<div style="clear: both;"></div>
-	<br>
-	@include('public.primera.partials._info-league')
 	<div style="clear: both;"></div>
 	<br>
+	@include('public.b_nacional.partials._info-league')
 	<div style="clear: both;"></div>
-	<br>
-	@include('public.primera.partials._associated_skills')
-	<div style="clear: both;"></div>
-	<br>
-	@include('public.primera.partials._positions')
-	@include('public.primera.partials._phases');
 	<br>
 	<div style="clear: both;"></div>
 	<br>
-	@include('public.primera.partials._averages')
-	@include('public.primera.partials._scorers')
+	@include('public.b_nacional.partials._associated_skills')
+	<div style="clear: both;"></div>
+	<br>
+	@include('public.b_nacional.partials._positions')
+	@include('public.b_nacional.partials._phases');
+	<br>
+	<div style="clear: both;"></div>
+	<br>
+	@include('public.b_nacional.partials._averages')
+	@include('public.b_nacional.partials._scorers')
 	<br>
 	<div style="clear: both;"></div>
 	<br>
 	<br>
 	<div style="clear: both;"></div>
 	<br>
-	@include('public.primera.partials._table-games-phase-tpl')
-	@include('public.primera.partials._name-phases-for-competition-tpl')
-	@include('public.primera.partials._stats-phase-tpl')
-	@include('public.primera.partials._scorers-tpl')
-	@include('public.primera.partials._average-head-tpl')
+	@include('public.b_nacional.partials._table-games-phase-tpl')
+	@include('public.b_nacional.partials._name-phases-for-competition-tpl')
+	@include('public.b_nacional.partials._stats-phase-tpl')
+	@include('public.b_nacional.partials._scorers-tpl')
+	@include('public.b_nacional.partials._average-head-tpl')
 @stop
 
 @section('scripts')
