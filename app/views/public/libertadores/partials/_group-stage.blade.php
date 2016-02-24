1f115fc5fd3e6a97f96ebd3ab6f248c588c35f60
ajpadilla <ajpadilla88@gmail.com>
2016-02-24 15:31:54 -0430

    Mostrando partidos de repechajes para libertadores

--- app/views/public/libertadores/partials/_group-stage.blade.php
+++ app/views/public/libertadores/partials/_group-stage.blade.php
@@ -2,8 +2,59 @@
     @foreach($libertadoresCups as $libertadoresCup)
         @if($libertadoresCup->hasGames)
             <?php $firstPhase = $libertadoresCup->phases()->whereType('fase de grupos')->first(); ?>
-
         
+        @if (!empty($firstPhaseRepechaje))
+           <div id="infoequipo">REPECHAJE</div>
+            <div id="fixrepe">
+                @foreach ($firstPhaseRepechaje->groups as $indexGroup => $group)
+                <div style="width: 400px; float: left; margin-top:10px">
+                    <table>
+                        <tbody>
+                             <tr>
+                                <td colspan="4">
+                                    <span class="datosequipo" style="display: block; background: url(assets/img/public/caja.png);width: 386px">
+                                        <span style="margin-left:10px; color: yellow">G{{ $indexGroup+1}}</span>
+                                        <span style="margin-left:50px">{{ $group->name }}</span>
+                                    </span>
+                                </td>
+                            </tr>
+                            @if (!empty($gamesForRepechaje))
+                                @foreach($gamesForRepechaje as $index => $groups)
+                                @foreach($groups as $index2 => $games)
+                                @foreach($games as $index2 => $game)
+                                @if($game['group_id'] == $group->id)
+                                <tr style="background:#155219">
+                                    <td colspan="4">
+                                    <span class="horariopartido">{{ $game['dateObject']->toFormattedDateString() }}  , {{ $game['time'] }}hs</span>
+                                        <span class="horariopartido2" style="float: right;">
+                                            <!--<span class="statuspartido2">Final</span> </span>-->
+                                        </td>
+                                    </tr>
+                                    <tr id="188" style="background: url(assets/img/public/caja2b.png)"><td width="150">
+                                        <span class="datoequipo"> {{ $game['localTeam']->nombre }}</span></td>
+                                        <td width="20" style="background: white">
+                                            <span class="datoequipo" style="margin-left: 5px">{{$game['localGoals'] }}</span>
+                                        </td>
+                                        <td width="20" style="background: white">
+                                            <span class="datoequipo" style="margin-left: 5px">{{ $game['awayGoals'] }} </span>
+                                        </td>
+                                        <td width="150"><span class="datoequipo">{{ $game['awayTeam']->nombre }}</span></td>
+                                    </tr>
+                                @endif
+                                @endforeach
+                                @endforeach
+                                @endforeach
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+                @endforeach
+            </div>
+            <br>
+            <div style="clear: both"></div>
+            <div style="clear: both"></div>
+            <br>
+        @endif
 
 
         @if(!empty($firstPhase))
