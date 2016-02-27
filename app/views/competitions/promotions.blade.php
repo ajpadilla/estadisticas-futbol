3179f24a13e5c199c1984e87cf64221a084b3251
ajpadilla <ajpadilla88@gmail.com>
2016-02-26 22:20:52 -0430

    Agregando blade para mostrar pop-up para crear ascensos para competencias

---
+++ app/views/competitions/promotions.blade.php
@@ -0,0 +1,9 @@
+<div class="row">
+	<div class="col-md-2">
+		<button class="pull-left btn btn-lg btn-primary" id="add-promotions-to-competition" data-competition-id="{{ $competition->id }}" href="#">Agregar Ascensos</button>
+	</div>
+	<div id="add-promotions-to-competition" class="hidden">
+			@include('competitions.partials._form_promotions')
+	</div>
+</div>
+<br />
