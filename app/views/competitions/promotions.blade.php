ef496f8adf45f9cb66041712b209729f92f064ba
ajpadilla <ajpadilla88@gmail.com>
2016-03-07 13:54:21 -0430

    Actializando blade

--- app/views/competitions/promotions.blade.php
+++ app/views/competitions/promotions.blade.php
@@ -3,7 +3,15 @@
 		<button class="pull-left btn btn-lg btn-primary" id="add-promotions-to-competition" data-competition-id="{{ $competition->id }}" href="#">Agregar Ascensos</button>
 	</div>
 	<div id="add-promotions-to-competition" class="hidden">
-			@include('competitions.partials._form_promotions')
+		@include('competitions.partials._form_promotions')
+	</div>
+
+</div>
+<div class="row">
+	<?php $table = $teamsTable; ?>
+	<div class="col-md-12">
+		@include('partials._index-table')
 	</div>
 </div>
 <br />
+
